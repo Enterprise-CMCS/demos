6a359c2568c84994355f85458bf26af7874064b9
Dustin <Dustin.H@globalalliantinc.com>
2025-05-30 12:25:48 -0400

    DEMOS-27: adding Demos table to the demos tab that Lucas created.
    still needs tests and some styles, but we are making progress

--- client/src/pages/Demonstrations.tsx
+++ client/src/pages/Demonstrations.tsx
@@ -1,11 +1,28 @@
-import React from "react";
+import React, { Fragment } from "react";
+import { DemonstrationColumns } from "components/table/columns/DemonstrationColumns";
+import { TableSection } from "components/table/sections/TableSection";
+import DemonstrationTable from "components/table/tables/DemonstrationTable";
+import { Header, Main, Footer } from "components";
+import DemoData from "faker_data/demonstrations.json";
+
 
 const Demonstrations: React.FC = () => {
   return (
-    <div className="p-6">
-      <h1 className="text-2xl font-bold">Demonstrations Page</h1>
-      <p>This is just a placeholder to verify side nav routing is working.</p>
-    </div>
+    <Fragment>
+      <Header />
+      <Main>
+        <div className="p-6">
+          <h1 className="text-2xl font-bold">Demonstrations</h1>
+          <TableSection title="Demonstrations">
+            <DemonstrationTable
+              data={DemoData}
+              columns={DemonstrationColumns}
+            />
+          </TableSection>
+        </div>
+      </Main>
+      <Footer />
+    </Fragment>
   );
 };
 
