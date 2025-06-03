78c932e1632a838f2ad844466a3a9f86d73d8736
Tresdon Jones <tresdon.jones@icf.com>
2025-06-03 17:28:04 -0400

    Fix router

--- client/src/router/DemosRouter.tsx
+++ client/src/router/DemosRouter.tsx
@@ -4,19 +4,10 @@ import { AuthProvider } from "react-oidc-context";
 import { getCognitoConfig } from "./cognitoConfig";
 import { LandingPage } from "pages";
 import { ComponentLibrary, TestHooks } from "pages/debug";
-import { AuthComponent } from "components/auth/AuthComponent";
+import { AuthComponent } from "components/debug/AuthComponent";
 import { PrimaryLayout } from "layout/PrimaryLayout";
 
 // Routes that are only available in development mode
-const DebugRoutes = () => {
-  return (
-    <>
-      <Route path="/components" element={<ComponentLibrary />} />
-      <Route path="/hooks" element={<TestHooks />} />
-    </>
-  );
-};
-
 export const DemosRouter = () =>  {
   const cognitoConfig = getCognitoConfig();
 
@@ -27,7 +18,13 @@ export const DemosRouter = () =>  {
           <Route path="/" element={<PrimaryLayout><LandingPage /></PrimaryLayout>} />
           <Route path="/login" element={<AuthComponent />} />
           <Route path="/login-redirect" element={<AuthComponent />} />
-          {process.env.NODE_ENV === "development" && <DebugRoutes />}
+          {/* Debug routes, only available in development mode */}
+          {process.env.NODE_ENV === "development" && (
+            <>
+              <Route path="/components" element={<ComponentLibrary />} />
+              <Route path="/hooks" element={<TestHooks />} />
+            </>
+          )}
         </Routes>
       </BrowserRouter>
     </AuthProvider>
