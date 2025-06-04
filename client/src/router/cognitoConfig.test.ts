c01a69373aa37a5e24479ccda2577ebfd865e570
Tresdon Jones <tresdon.jones@icf.com>
2025-06-04 12:26:10 -0400

    Remove noisy test

--- client/src/router/cognitoConfig.test.ts
+++ client/src/router/cognitoConfig.test.ts
@@ -1,12 +1,5 @@
 import { describe, it, expect, vi, beforeEach, afterEach } from "vitest";
-import {
-
-  LOCAL_COGNITO_CONFIG,
-  getCognitoLogoutUrl,
-  logout,
-  getCognitoConfig,
-  CognitoConfig
-} from "./cognitoConfig";
+import { LOCAL_COGNITO_CONFIG, getCognitoLogoutUrl, getCognitoConfig } from "./cognitoConfig";
 
 describe("cognitoConfig", () => {
   describe("getCognitoLogoutUrl", () => {
@@ -52,30 +45,6 @@ describe("cognitoConfig", () => {
       expect(() => getCognitoConfig()).toThrow(
         "Cognito configuration for staging is not defined."
       );
-    });
-  });
-
-  describe("logout", () => {
-    let originalLocation: Location;
-
-    beforeEach(() => {
-      originalLocation = window.location;
-      // @ts-ignore
-      delete window.location;
-      // @ts-ignore
-      window.location = { href: "" };
-      process.env.NODE_ENV = "development";
-    });
-
-    afterEach(() => {
-      window.location = originalLocation;
-    });
-
-    it("should redirect to the Cognito logout URL", () => {
-      logout();
-      const expectedUrl =
-      "https://us-east-1a7car2wo3.auth.us-east-1.amazoncognito.com/logout?client_id=5km9thunj8g6qd32s5et2i8pga&logout_uri=http%3A%2F%2Flocalhost%3A3000";
-      expect(window.location.href).toBe(expectedUrl);
     });
   });
 });
