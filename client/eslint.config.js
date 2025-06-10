91950f84201e25cffab69f469d724cdc05d26785
Tresdon Jones <tresdon.jones@icf.com>
2025-06-10 15:27:25 -0400

    Initial Commit

--- client/eslint.config.js
+++ client/eslint.config.js
@@ -1,12 +1,14 @@
 import jsEslint from "@eslint/js";
 import tseslint from "typescript-eslint";
 import reactEslint from "eslint-plugin-react";
+import prettierConfig from "eslint-config-prettier";
 import { noRelativeComponentImports } from "./eslint-rules/no-relative-component-imports.js";
 
 const eslintConfig = tseslint.config(
   jsEslint.configs.recommended,
   tseslint.configs.recommended,
   reactEslint.configs.flat.recommended,
+  prettierConfig,
   {
     settings: {
       react: {
@@ -19,15 +21,7 @@ const eslintConfig = tseslint.config(
       },
     },
     rules: {
-      "indent": [ "error", 2 ], // 2 spaces for indentation
-      "quotes": [ "error", "double" ], // Double quotes
-      "semi": [ "error", "always" ], // Semicolons at end of statements
-      "no-trailing-spaces": "error", // Disallow trailing spaces
-      "no-tabs": "error", // Disallow tabs for indentation
-      "eol-last": [ "error", "always" ], // Newline at the end of files
-      "comma-dangle": [ "error", { "arrays": "always-multiline", "objects": "always-multiline" } ], // Trailing commas in multiline arrays and objects
       "no-restricted-exports": ["error", { "restrictDefaultExports": { "direct": true } }], // Disallow default exports
-      "object-curly-spacing": [ "error", "always" ], // Spaces inside object curly braces
       "no-relative-component-imports/no-relative-component-imports": "error", // Disallow relative imports of components
     },
   }
