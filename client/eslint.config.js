6089b34b414e7baa03fda1e5e60b6d509fd58d7d
Tresdon Jones <tresdon.jones@icf.com>
2025-06-11 11:26:47 -0400

    Add eslint rules back

--- client/eslint.config.js
+++ client/eslint.config.js
@@ -1,7 +1,7 @@
 import jsEslint from "@eslint/js";
 import tseslint from "typescript-eslint";
 import reactEslint from "eslint-plugin-react";
-import prettierConfig from "eslint-config-prettier";
+import prettierConfig from "eslint-config-prettier/flat";
 import { noRelativeComponentImports } from "./eslint-rules/no-relative-component-imports.js";
 
 const eslintConfig = tseslint.config(
@@ -21,7 +21,20 @@ const eslintConfig = tseslint.config(
       },
     },
     rules: {
-      "no-restricted-exports": ["error", { "restrictDefaultExports": { "direct": true } }], // Disallow default exports
+      indent: ["error", 2], // 2 spaces for indentation
+      quotes: ["error", "double"], // Double quotes
+      semi: ["error", "always"], // Semicolons at end of statements
+      "no-trailing-spaces": "error", // Disallow trailing spaces
+      "no-tabs": "error", // Disallow tabs for indentation
+      "eol-last": ["error", "always"], // Newline at the end of files
+      "comma-dangle": [
+        "error",
+        { arrays: "always-multiline", objects: "always-multiline" },
+      ], // Trailing commas in multiline arrays and objects
+      "no-restricted-exports": [
+        "error",
+        { restrictDefaultExports: { direct: true } },
+      ], // Disallow default exports
       "no-relative-component-imports/no-relative-component-imports": "error", // Disallow relative imports of components
     },
   }
