84aff5276f5140338ed1789450076578c96f6ae7
Tresdon Jones <tresdon.jones@icf.com>
2025-06-13 12:20:02 -0400

    Fix users query

--- client/src/hooks/useUserOperations.ts
+++ client/src/hooks/useUserOperations.ts
@@ -4,8 +4,7 @@ import { User } from "demos-server";
 export const GET_ALL_USERS = gql`
   query GetUsers {
     users {
-      firstName
-      lastName
+      fullName
     }
   }
 `;
@@ -13,9 +12,7 @@ export const GET_ALL_USERS = gql`
 export const GET_USER_BY_ID = gql`
   query GetUser($id: ID!) {
     user(id: $id) {
-      id
-      firstName
-      lastName
+      fullName
     }
   }
 `;
