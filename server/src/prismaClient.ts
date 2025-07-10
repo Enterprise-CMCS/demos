603d330c2cf4842953bd412036af8ae7869b283e
Tresdon Jones <tresdon.jones@icf.com>
2025-07-10 14:37:11 -0400

    Prisma client fix

--- server/src/prismaClient.ts
+++ server/src/prismaClient.ts
@@ -1,7 +1,7 @@
 import { PrismaClient } from "@prisma/client";
 import prismaRandom from "prisma-extension-random";
 
-export let prisma: any = undefined; // eslint-disable-line -- temporary until solution is developed
+export let prisma = new PrismaClient().$extends(prismaRandom());
 export const initializeClient = () => {
   if (!prisma) {
     prisma = new PrismaClient().$extends(prismaRandom());
