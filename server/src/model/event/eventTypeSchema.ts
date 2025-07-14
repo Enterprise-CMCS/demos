919d2f47693dc37eef2dfb43ee68f5f06e46a699
Tresdon Jones <tresdon.jones@icf.com>
2025-07-14 16:49:07 -0400

    DEMOS-322: Server-Side Event Logging Prototype (#73)

---
+++ server/src/model/event/eventTypeSchema.ts
@@ -0,0 +1,23 @@
+import { gql } from "graphql-tag";
+
+/**
+ * EventType represents a type of event that can be logged from the UI.
+ * This is a fact / reference table and it should be populated via DDL /
+ * migrations rather than created dynamically through the UI.
+ */
+export const eventTypeSchema = gql`
+  type EventType {
+    id: ID! # A natural key, doubles as the "name" of the event
+    description: String! # A description of what this event type represents
+  }
+
+  type Query {
+    eventTypes: [EventType!]!
+    eventType(id: ID!): EventType
+  }
+`;
+
+export interface EventType {
+  id: string;
+  description: string;
+}
