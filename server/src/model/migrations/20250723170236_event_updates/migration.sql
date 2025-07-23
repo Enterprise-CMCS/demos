d17262dc4458c16464b40e25dff31794a412a877
Tresdon Jones <tresdon.jones@icf.com>
2025-07-23 13:04:50 -0400

    Regenerate migration with onDelete setNull

--- server/src/model/migrations/20250723170236_event_updates/migration.sql
+++ server/src/model/migrations/20250723170236_event_updates/migration.sql
@@ -9,7 +9,6 @@
   - You are about to drop the `log_level` table. If the table is not empty, all the data it contains will be lost.
   - Added the required column `event_type` to the `event` table without a default value. This is not possible if the table is not empty.
   - Added the required column `log_level` to the `event` table without a default value. This is not possible if the table is not empty.
-  - Added the required column `with_role_id` to the `event` table without a default value. This is not possible if the table is not empty.
 
 */
 -- DropForeignKey
@@ -29,13 +28,14 @@ DROP COLUMN "log_level_id",
 DROP COLUMN "role_id",
 ADD COLUMN     "event_type" TEXT NOT NULL,
 ADD COLUMN     "log_level" TEXT NOT NULL,
-ADD COLUMN     "with_role_id" UUID NOT NULL;
+ADD COLUMN     "with_role_id" UUID,
+ALTER COLUMN "user_id" DROP NOT NULL;
 
 -- DropTable
 DROP TABLE "log_level";
 
 -- AddForeignKey
-ALTER TABLE "event" ADD CONSTRAINT "event_with_role_id_fkey" FOREIGN KEY ("with_role_id") REFERENCES "role"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "event" ADD CONSTRAINT "event_with_role_id_fkey" FOREIGN KEY ("with_role_id") REFERENCES "role"("id") ON DELETE SET NULL ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "event" ADD CONSTRAINT "event_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "event" ADD CONSTRAINT "event_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE SET NULL ON UPDATE CASCADE;
