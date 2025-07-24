41e2d67aa5bb9b29ed4b772947b3d9fe8224d399
Tresdon Jones <tresdon.jones@icf.com>
2025-07-24 16:25:15 -0400

    Fix Error: Query.event defined in resolvers, but not in schema

--- server/src/model/event/eventResolvers.ts
+++ server/src/model/event/eventResolvers.ts
@@ -1,32 +1,11 @@
 import { prisma } from '../../prismaClient.js';
 import { LogEventInput } from './eventSchema.js';
-import { Event } from "@prisma/client";
 import { GraphQLContext, getCurrentUserId, getCurrentUserRoleId } from '../../auth/auth.util.js';
 
 export const eventResolvers = {
   Query: {
     events: async () => {
       return await prisma().event.findMany({
-        orderBy: { createdAt: 'desc' }
-      });
-    },
-
-    event: async (_: undefined, { id }: { id: string }) => {
-      return await prisma().event.findUnique({
-        where: { id }
-      });
-    },
-
-    eventsByType: async (_: undefined, { eventType }: { eventType: string }) => {
-      return await prisma().event.findMany({
-        where: { eventType },
-        orderBy: { createdAt: 'desc' }
-      });
-    },
-
-    eventsByRoute: async (_: undefined, { route }: { route: string }) => {
-      return await prisma().event.findMany({
-        where: { route },
         orderBy: { createdAt: 'desc' }
       });
     }
@@ -58,19 +37,4 @@ export const eventResolvers = {
       }
     }
   },
-
-  Event: {
-    user: async (parent: Event) => {
-      return await prisma().user.findUnique({
-        where: { id: parent.userId },
-      });
-    },
-
-    withRole: async (parent: Event) => {
-      return await prisma().role.findUnique({
-        where: { id: parent.withRoleId },
-      });
-    },
-  }
-
 };
