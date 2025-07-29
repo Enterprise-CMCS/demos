ca49dcb26cf78cea612cb1205faa86b96841f960
Tresdon Jones <tresdon.jones@icf.com>
2025-07-29 14:20:11 -0400

    Fix linting errors

--- client/src/components/button/BaseButton.tsx
+++ client/src/components/button/BaseButton.tsx
@@ -27,18 +27,18 @@ export const BaseButton: React.FC<BaseButtonProps> = ({
 
   const sizeClass = isCircle
     ? {
-        large: "w-14 h-14 text-[1.4rem] font-semibold leading-none",
-        small: "w-8 h-8 text-[1rem] font-medium leading-none",
-        standard: "w-12 h-12 text-[1.2rem] font-semibold leading-none",
-      }[size]
+      large: "w-14 h-14 text-[1.4rem] font-semibold leading-none",
+      small: "w-8 h-8 text-[1rem] font-medium leading-none",
+      standard: "w-12 h-12 text-[1.2rem] font-semibold leading-none",
+    }[size]
     : {
-        large:
+      large:
           "text-[1.6rem] font-semibold px-6 py-3 leading-tight tracking-wide",
-        small:
+      small:
           "text-[1.2rem] font-medium px-1 py-1 leading-tight tracking-wide",
-        standard:
+      standard:
           "text-[1.4rem] font-medium px-4 py-2 leading-tight tracking-wide",
-      }[size];
+    }[size];
 
   const base =
     "inline-flex items-center justify-center focus:outline-none transition-all disabled:opacity-50 cursor-pointer disabled:cursor-not-allowed";
