aeb0e2e74109fdd5735ca955f3834ff4721fae8d
Tresdon Jones <tresdon.jones@icf.com>
2025-07-29 14:16:45 -0400

    DocumentTable updates

--- client/src/components/button/BaseButton.tsx
+++ client/src/components/button/BaseButton.tsx
@@ -9,6 +9,7 @@ interface BaseButtonProps {
   disabled?: boolean;
   onClick?: React.MouseEventHandler<HTMLButtonElement>;
   className?: string;
+  ariaLabel?: string;
   children: React.ReactNode;
 }
 
@@ -19,21 +20,25 @@ export const BaseButton: React.FC<BaseButtonProps> = ({
   disabled = false,
   onClick,
   className = "",
+  ariaLabel,
   children,
 }) => {
   const isCircle = className?.includes("rounded-full");
 
   const sizeClass = isCircle
     ? {
-      large: "w-14 h-14 text-[1.4rem] font-semibold leading-none",
-      small: "w-8 h-8 text-[1rem] font-medium leading-none",
-      standard: "w-12 h-12 text-[1.2rem] font-semibold leading-none",
-    }[size]
+        large: "w-14 h-14 text-[1.4rem] font-semibold leading-none",
+        small: "w-8 h-8 text-[1rem] font-medium leading-none",
+        standard: "w-12 h-12 text-[1.2rem] font-semibold leading-none",
+      }[size]
     : {
-      large: "text-[1.6rem] font-semibold px-6 py-3 leading-tight tracking-wide",
-      small: "text-[1.2rem] font-medium px-1 py-1 leading-tight tracking-wide",
-      standard: "text-[1.4rem] font-medium px-4 py-2 leading-tight tracking-wide",
-    }[size];
+        large:
+          "text-[1.6rem] font-semibold px-6 py-3 leading-tight tracking-wide",
+        small:
+          "text-[1.2rem] font-medium px-1 py-1 leading-tight tracking-wide",
+        standard:
+          "text-[1.4rem] font-medium px-4 py-2 leading-tight tracking-wide",
+      }[size];
 
   const base =
     "inline-flex items-center justify-center focus:outline-none transition-all disabled:opacity-50 cursor-pointer disabled:cursor-not-allowed";
@@ -45,6 +50,7 @@ export const BaseButton: React.FC<BaseButtonProps> = ({
       className={`${base} ${sizeClass} ${className}`}
       disabled={disabled}
       onClick={onClick}
+      aria-label={ariaLabel}
     >
       {children}
     </button>
