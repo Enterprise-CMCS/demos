e5f5e0692b2b91fb860b6e0c74ef6b51c2659ff3
Connor J. Parke <connor.parke@icf.com>
2025-08-13 16:11:07 +0000

    demos-378: adjusted procedure transaction handling

--- server/src/model/migrations/20250804171333_add_documents_pending_upload_table/migration.sql
+++ server/src/model/migrations/20250804171333_add_documents_pending_upload_table/migration.sql
@@ -38,34 +38,51 @@ ALTER TABLE "document_pending_upload" ADD CONSTRAINT "document_pending_upload_ow
 -- AddForeignKey
 ALTER TABLE "document_pending_upload" ADD CONSTRAINT "document_pending_upload_bundle_id_fkey" FOREIGN KEY ("bundle_id") REFERENCES "bundle"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
-CREATE OR REPLACE PROCEDURE demos_app.move_document_from_processing_to_clean(
+CREATE OR REPLACE PROCEDURE
+demos_app.move_document_from_processing_to_clean(
     p_id UUID,
     p_s3_path TEXT
 )
 LANGUAGE plpgsql
 AS $$
 BEGIN
-    BEGIN
-        INSERT INTO demos_app.document (id, title, description, s3_path, owner_user_id, document_type_id, bundle_id, created_at, updated_at) 
-        SELECT 
-            id, title, description, p_s3_path, owner_user_id, document_type_id, bundle_id, created_at, updated_at
-        FROM 
-            demos_app.document_pending_upload 
-        WHERE id = p_id;
-        
-        IF NOT FOUND THEN
-            ROLLBACK;
-            RAISE EXCEPTION 'No document_pending_upload found for id: %', p_id;
-        END IF;
 
-        DELETE FROM demos_app.document_pending_upload
-        WHERE id = p_id;
+    IF NOT EXISTS (SELECT id FROM demos_app.document_pending_upload WHERE id = p_id) THEN
+        RAISE EXCEPTION 'No document_pending_upload found for id: %', p_id;
+    END IF;
+
+    INSERT INTO demos_app.document (
+        id,
+        title,
+        description,
+        s3_path,
+        owner_user_id,
+        document_type_id,
+        bundle_id,
+        created_at,
+        updated_at
+    )
+    SELECT
+        id,
+        title,
+        description,
+        p_s3_path,
+        owner_user_id,
+        document_type_id,
+        bundle_id,
+        created_at,
+        updated_at
+    FROM
+        demos_app.document_pending_upload
+    WHERE id = p_id;
+
+    DELETE FROM
+        demos_app.document_pending_upload
+    WHERE
+        id = p_id;
 
     EXCEPTION WHEN OTHERS THEN
-        ROLLBACK;
         RAISE EXCEPTION 'Failed to move document from processing to clean. Details: %', SQLERRM;
-    COMMIT;
-    END;
 END;
 $$;
 CREATE OR REPLACE FUNCTION demos_app.log_changes_document_pending_upload()
