98f04b790e7b87341289d8133cbf152f12225d6a
Dustin <Dustin.H@globalalliantinc.com>
2025-08-15 10:57:58 -0400

    DEMOS-450: removed commented out code

--- client/src/pages/DemonstrationDetail/DemonstrationTab.tsx
+++ client/src/pages/DemonstrationDetail/DemonstrationTab.tsx
@@ -2,7 +2,6 @@ import React, { useState } from "react";
 
 import { SecondaryButton } from "components/button";
 import { AddNewIcon } from "components/icons";
-// import { CreateNewModal } from "components/modal/CreateNewModal";
 import { DocumentTable } from "components/table/tables/DocumentTable";
 import { SummaryDetailsTable } from "components/table/tables/SummaryDetailsTable";
 import { AddDocumentModal } from "components/modal/document/DocumentModal";
