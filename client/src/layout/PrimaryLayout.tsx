0b8bf267659c336baae7908edd4b9af1ccc7131b
Dustin <Dustin.H@globalalliantinc.com>
2025-08-22 17:32:00 +0000

    DEMOS-623: needed to remove a userid

--- client/src/layout/PrimaryLayout.tsx
+++ client/src/layout/PrimaryLayout.tsx
@@ -37,7 +37,7 @@ export const PrimaryLayout: React.FC<PrimaryLayoutProps> = ({ children }) => {
         defaultLowerContent={<DefaultHeaderLower />}
       >
         <div className="h-screen flex flex-col">
-          <Header userId={userId} />
+          <Header />
           <div className="flex flex-1 overflow-hidden bg-gray-100">
             <div className={collapsed ? "w-20" : "w-64"}>
               <SideNav collapsed={collapsed} setCollapsed={setCollapsed} />
