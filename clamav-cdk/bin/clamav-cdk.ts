9ed91c0aff3b1dc2eacbe7a2d28d81503a77abaa
Jesse Toula <jesse.t@globalalliantinc.com>
2025-09-03 16:25:48 -0400

    Remove clamav-cdk directory

--- clamav-cdk/bin/clamav-cdk.ts
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env node
-import * as cdk from "aws-cdk-lib";
-
-import { ClamavCdkStack } from "../lib/clamav-cdk-stack";
-
-const app = new cdk.App();
-new ClamavCdkStack(app, "ClamavCdkStack", {});
