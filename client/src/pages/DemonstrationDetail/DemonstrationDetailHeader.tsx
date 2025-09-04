1eb7c5d452abd15fdcaee80e28cbe033ff3245a3
Connor J. Parke <connor.parke@icf.com>
2025-09-04 12:42:30 +0000

    DEMOS-681: removed stray code snippet

--- client/src/pages/DemonstrationDetail/DemonstrationDetailHeader.tsx
+++ client/src/pages/DemonstrationDetail/DemonstrationDetailHeader.tsx
@@ -62,9 +62,6 @@ export const DemonstrationDetailHeader: React.FC<DemonstrationDetailHeaderProps>
       </div>
     );
   }
-  {
-    date ? formatDate(date) : "--/--/----";
-  }
 
   const displayFields = [
     { label: "State/Territory", value: demonstration.state.id },
