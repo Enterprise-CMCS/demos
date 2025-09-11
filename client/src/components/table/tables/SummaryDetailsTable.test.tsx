49233cda02120f78c80ef767972ae5d32e770e15
Tresdon Jones <tresdon.jones@icf.com>
2025-09-11 16:29:48 -0400

    Remove unused import

--- client/src/components/table/tables/SummaryDetailsTable.test.tsx
+++ client/src/components/table/tables/SummaryDetailsTable.test.tsx
@@ -3,7 +3,7 @@ import React from "react";
 import { formatDate } from "util/formatDate";
 import { beforeEach, describe, expect, it, vi } from "vitest";
 
-import { render, screen, waitFor } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 
 import { SummaryDetailsTable } from "./SummaryDetailsTable";
