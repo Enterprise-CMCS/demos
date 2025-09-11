336ff60eff68b647a4747478cd4153b712e4f998
Connor J. Parke <connor.parke@icf.com>
2025-09-11 14:49:39 +0000

    DEMOS-722: adjusted roleSchema to be scaler

--- server/src/model/role/roleSchema.ts
+++ server/src/model/role/roleSchema.ts
@@ -1,7 +1,13 @@
 import { gql } from "graphql-tag";
 
 export const roleSchema = gql`
-  type Role {
-    id: ID!
-  }
+  """
+  A string representing the status of a phase of an application. Expected values are:
+  - State Point of Contact
+  - DDME Analyst
+  - Policy Technical Director
+  - Monitoring & Evaluation Technical Director
+  - All Users
+  """
+  scalar Role
 `;
