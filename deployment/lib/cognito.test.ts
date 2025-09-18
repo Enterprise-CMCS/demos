282c854d303f4458e4b51c87453764f9b355dad6
Dustin Horning <dustin.h@globalalliantinc.com>
2025-09-18 13:54:44 -0400

    DEMOS-529: adding some more test coverage to be > 80% (#258)
    * DEMOS-529: adding more test coverage
    
    Also removing the sign-out urls from the second half of the cognito file
    
    * DEMOS-529: centalized the duplicated code in cicd
    
    The cognito page has two functions that share a lot of code. SO i
    centalized it per Jesse's blessings
    
    * DEMOS-529: fixed linter error with an "any"
    
    * DEMOS-529: further refactor to remove duplication
    
    Got pinged with code replication sonarqube
    So i fixed it some more!
    
    * DEMOS-529: removed the suggested code smells
    
    the as const was failing.
    
    * DEMOS-529: trying to pass sonarqube

--- deployment/lib/cognito.test.ts
+++ deployment/lib/cognito.test.ts
@@ -1,6 +1,6 @@
 import { App, Stack } from "aws-cdk-lib";
 import { create, createUserPoolClient } from "./cognito";
-import { Template } from "aws-cdk-lib/assertions";
+import { Match, Template } from "aws-cdk-lib/assertions";
 
 const mockCommonProps = {
   project: "demos",
@@ -58,6 +58,105 @@ describe("Cognito", () => {
 
     template.hasResourceProperties("AWS::Cognito::UserPoolClient", {
       ClientName: "demos-unittest-user-pool-client",
+    });
+  });
+
+  test("includes IDM logout URL in dev", () => {
+    const app = new App();
+    const stack = new Stack(app, "DevStack");
+
+    create({
+      ...mockCommonProps,
+      scope: stack,
+      stage: "dev",
+      isDev: true,
+      cloudfrontHost: "dev.example.com",
+    });
+
+    const template = Template.fromStack(stack);
+    template.hasResourceProperties("AWS::Cognito::UserPoolClient", {
+      LogoutURLs: Match.arrayWith([
+        "https://dev.example.com/",
+        "http://localhost:3000/",
+        "https://test.idp.idm.cms.gov/login/signout",
+      ]),
+    });
+  });
+
+  test("excludes IDM logout URL in prod", () => {
+    const app = new App();
+    const stack = new Stack(app, "ProdStack");
+
+    create({
+      ...mockCommonProps,
+      scope: stack,
+      stage: "prod",
+      isDev: false,
+      isEphemeral: false,
+      cloudfrontHost: "prod.example.com",
+    });
+
+    const template = Template.fromStack(stack);
+    template.hasResourceProperties("AWS::Cognito::UserPoolClient", {
+      LogoutURLs: Match.arrayWith(["https://prod.example.com/"]),
+    });
+    template.hasResourceProperties("AWS::Cognito::UserPoolClient", {
+      LogoutURLs: Match.not(Match.arrayWith(["https://test.idp.idm.cms.gov/login/signout"])),
+    });
+  });
+
+  test("createUserPoolClient includes IDM logout URL in dev", () => {
+    const app = new App();
+    const stack = new Stack(app, "ImportDevStack");
+
+    createUserPoolClient(
+      {
+        ...mockCommonProps,
+        scope: stack,
+        stage: "dev",
+        isDev: true,
+        isEphemeral: false,
+        cloudfrontHost: "dev.example.com",
+      },
+      "pool-id-placeholder",
+      "dev"
+    );
+
+    const template = Template.fromStack(stack);
+    template.hasResourceProperties("AWS::Cognito::UserPoolClient", {
+      LogoutURLs: Match.arrayWith([
+        "https://dev.example.com/",
+        "http://localhost:3000/",
+        "https://test.idp.idm.cms.gov/login/signout",
+      ]),
+    });
+  });
+
+  test("createUserPoolClient excludes IDM logout URL in prod", () => {
+    const app = new App();
+    const stack = new Stack(app, "ImportProdStack");
+
+    createUserPoolClient(
+      {
+        ...mockCommonProps,
+        scope: stack,
+        stage: "prod",
+        isDev: false,
+        isEphemeral: false,
+        cloudfrontHost: "prod.example.com",
+      },
+      "pool-id-placeholder",
+      "prod"
+    );
+
+    const template = Template.fromStack(stack);
+    template.hasResourceProperties("AWS::Cognito::UserPoolClient", {
+      LogoutURLs: Match.arrayWith([
+        "https://prod.example.com/",
+      ]),
+    });
+    template.hasResourceProperties("AWS::Cognito::UserPoolClient", {
+      LogoutURLs: Match.not(Match.arrayWith(["https://test.idp.idm.cms.gov/login/signout"])),
     });
   });
 });
