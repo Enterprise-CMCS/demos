498cdab7cfd7ec305f4a12f458b5fd19d1c22ab4
Jeff <JeffreyM@globalalliantinc.com>
2025-09-23 13:53:23 +0000

    DEMOS-684 add sample demo to test

--- client/src/components/application/ApplicationWorkflow.test.tsx
+++ client/src/components/application/ApplicationWorkflow.test.tsx
@@ -9,9 +9,49 @@ import { ApplicationWorkflow } from "./ApplicationWorkflow";
 
 describe("ApplicationWorkflow", () => {
   it("renders APPLICATION heading", () => {
+    const demonstration = {
+      id: "fcf8d9f9-03ff-4092-b784-937a760e5f5b",
+      name: "decretum sortitus strues",
+      description: "Defendo via delibero vitae umquam sufficio bestia.",
+      effectiveDate: "2026-07-18T00:00:00.000Z",
+      expirationDate: "2026-03-27T00:00:00.000Z",
+      state: {
+        id: "OR",
+        name: "Oregon",
+      },
+      status: "Under Review" as const,
+      projectOfficer: {
+        fullName: "Virginia McClure",
+      },
+      amendments: [],
+      extensions: [],
+      documents: [
+        {
+          id: "db013f55-734c-4863-92d0-322bbbd72d55",
+          title: "Curatio valeo.",
+          description: "Application for Curatio valeo.",
+          documentType: {
+            id: "State Application",
+          },
+          createdAt: "2025-09-19T18:05:41.071Z",
+          owner: {
+            fullName: "Marion Bergnaum",
+          },
+        },
+      ],
+      contacts: [
+        {
+          id: "1",
+          fullName: "John Doe",
+          email: "john.doe@email.com",
+          contactType: "Project Officer",
+        },
+      ],
+      currentPhase: "Federal Comment" as const,
+    };
     render(
       <TestProvider>
-        <ApplicationWorkflow demonstration={{ status: "Under Review" }} />
+        <ApplicationWorkflow demonstration={demonstration} />
       </TestProvider>
     );
     expect(screen.getByText("APPLICATION")).toBeInTheDocument();
