5f0bbc3cb5e0349b1f8baf43377c4a4359dd516d
Jesse Toula <jesse.t@globalalliantinc.com>
2025-10-08 09:50:00 -0400

    update cdk and server pipelines

--- pipelines/lib/vars/hasChange.groovy
+++ pipelines/lib/vars/hasChange.groovy
@@ -4,6 +4,11 @@ import groovy.transform.Field
 
 def call(String path, String target = "") {
 
+  if (!env.CHANGE_ID) {
+    echo "Always trigger if pipeline run is not a pull request"
+    return true
+  }
+
   if (changes.containsKey(path)) {
     return changes[path]
   }
@@ -13,6 +18,7 @@ def call(String path, String target = "") {
   }
   
   def diffStatus = sh(script: "git diff --name-only --merge-base origin/${target} -- ${path}", returnStdout: true).trim()
+  echo "Diff status for path '${path}': ${diffStatus}"
   changes[path] = diffStatus != ""
   return changes[path]
 }
