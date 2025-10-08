e0a66715f7cfda3322206e57fc47582d7bbd19ab
Jesse Toula <jesse.t@globalalliantinc.com>
2025-10-08 09:00:47 -0400

    cache change

--- pipelines/lib/vars/hasChange.groovy
+++ pipelines/lib/vars/hasChange.groovy
@@ -1,8 +1,19 @@
+import groovy.transform.Field
+
+@Field Map<String, Boolean> changes = [:]
+
 def call(String path, String target = "") {
+
+  if (changes.containsKey(path)) {
+    echo "cache hit"
+    return changes[path]
+  }
+
   if (target == "") {
     target = env.CHANGE_TARGET
   }
   
   def diffStatus = sh(script: "git diff --name-only --merge-base origin/${target} -- ${path}", returnStdout: true).trim()
-  return diffStatus != ""
+  changes[path] = diffStatus != ""
+  return changes[path]
 }
