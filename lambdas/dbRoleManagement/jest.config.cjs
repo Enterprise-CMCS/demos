471974e154ccf250a797501d15b2b8e119e2a48e
cms-jesse <107690848+cms-jesse@users.noreply.github.com>
2025-10-08 15:51:02 -0400

    DEMOS-885: reorganize lambda authorizer (#300)
    * DEMOS-885: Move authorizer lambda to lambdas directory
    
    * Add unit testing to lambdas
    
    * Update deployment pipeline to include lambda testing
    
    * Reference ts file for fileprocess
    
    * Remove lambda scans from CDK pipeline
    
    These scans are now included in the lambda pipeline
    
    * Update lambda pipeline with conditionals

--- lambdas/dbRoleManagement/jest.config.cjs
+++ lambdas/dbRoleManagement/jest.config.cjs
@@ -2,6 +2,7 @@
 
 process.env.STAGE = "unit-test";
 process.env.REGION = "us-east-1";
+process.env.DATABASE_SECRET_ARN = "fake-secret"; // pragma: allowlist secret
 
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
