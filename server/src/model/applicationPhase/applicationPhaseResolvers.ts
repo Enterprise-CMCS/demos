b727c670e0abdbbbeef9aaf891de07d8caa951f2
Connor J. Parke <connor.parke@icf.com>
2025-10-27 15:35:53 +0000

    DEMOS-1025: added application phase status resolver

--- server/src/model/applicationPhase/applicationPhaseResolvers.ts
+++ server/src/model/applicationPhase/applicationPhaseResolvers.ts
@@ -1,5 +1,24 @@
 import { ApplicationPhase } from "@prisma/client";
 import { getApplicationDatesForPhase } from "../applicationDate/applicationDateResolvers.js";
+import { prisma } from "../../prismaClient.js";
+import { SetApplicationPhaseStatusInput } from "./applicationPhaseSchema.js";
+
+export async function setApplicationPhaseStatus(
+  _: undefined,
+  { input }: { input: SetApplicationPhaseStatusInput }
+) {
+  return await prisma().applicationPhase.update({
+    where: {
+      applicationId_phaseId: {
+        applicationId: input.applicationId,
+        phaseId: input.phaseName,
+      },
+    },
+    data: {
+      phaseStatusId: input.phaseStatus,
+    },
+  });
+}
 
 export const applicationPhaseResolvers = {
   ApplicationPhase: {
@@ -12,5 +31,14 @@ export const applicationPhaseResolvers = {
     phaseDates: async (parent: ApplicationPhase) => {
       return getApplicationDatesForPhase(parent.applicationId, parent.phaseId);
     },
+    application: async (parent: ApplicationPhase) => {
+      return await prisma().application.findUnique({
+        where: { id: parent.applicationId },
+      });
+    }
+  },
+
+  Mutation: {
+    setApplicationPhaseStatus: setApplicationPhaseStatus,
   },
 };
--- server/src/model/applicationPhase/applicationPhaseSchema.ts
+++ server/src/model/applicationPhase/applicationPhaseSchema.ts
@@ -1,4 +1,4 @@
-import { PhaseName, PhaseStatus, ApplicationDate } from "../../types.js";
+import { PhaseName, PhaseStatus, ApplicationDate, Application } from "../../types.js";
 import { gql } from "graphql-tag";
 
 export const applicationPhaseSchema = gql`
@@ -8,6 +8,17 @@ export const applicationPhaseSchema = gql`
     phaseDates: [ApplicationDate!]!
     createdAt: DateTime!
     updatedAt: DateTime!
+    application: Application!
+  }
+
+  input SetApplicationPhaseStatusInput {
+    applicationId: ID!
+    phaseName: PhaseName!
+    phaseStatus: PhaseStatus!
+  }
+
+  type Mutation {
+    setApplicationPhaseStatus(input: SetApplicationPhaseStatusInput!): ApplicationPhase
   }
 `;
 
@@ -17,4 +28,11 @@ export interface ApplicationPhase {
   phaseDates: ApplicationDate[];
   createdAt: Date;
   updatedAt: Date;
+  application: Application;
+}
+
+export interface SetApplicationPhaseStatusInput {
+  applicationId: string;
+  phaseName: PhaseName;
+  phaseStatus: PhaseStatus;
 }
