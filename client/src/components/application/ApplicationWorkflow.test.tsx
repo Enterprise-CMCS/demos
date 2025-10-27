e8616f054d0c720ff800572d79cf0f7ca02ee157
Tresdon Jones <tresdon.jones@icf.com>
2025-10-27 12:28:55 -0400

    Query for the demonstration, remove mock stuff

--- client/src/components/application/ApplicationWorkflow.test.tsx
+++ client/src/components/application/ApplicationWorkflow.test.tsx
@@ -3,57 +3,21 @@ import React from "react";
 import { TestProvider } from "test-utils/TestProvider";
 import { describe, expect, it } from "vitest";
 
-import { render, screen } from "@testing-library/react";
+import { render, screen, waitFor } from "@testing-library/react";
 
 import { ApplicationWorkflow } from "./ApplicationWorkflow";
 
+const MOCK_DEMONSTRATION_ID = "mockDemonstrationId";
+
 describe("ApplicationWorkflow", () => {
   it("renders APPLICATION heading", () => {
-    const demonstration = {
-      id: "fcf8d9f9-03ff-4092-b784-937a760e5f5b",
-      name: "decretum sortitus strues",
-      description: "Defendo via delibero vitae umquam sufficio bestia.",
-      effectiveDate: "2026-07-18T00:00:00.000Z",
-      expirationDate: "2026-03-27T00:00:00.000Z",
-      state: {
-        id: "OR",
-        name: "Oregon",
-      },
-      status: "Under Review" as const,
-      projectOfficer: {
-        fullName: "Virginia McClure",
-      },
-      amendments: [],
-      extensions: [],
-      documents: [
-        {
-          id: "db013f55-734c-4863-92d0-322bbbd72d55",
-          title: "Curatio valeo.",
-          description: "Application for Curatio valeo.",
-          documentType: {
-            id: "State Application",
-          },
-          createdAt: "2025-09-19T18:05:41.071Z",
-          owner: {
-            fullName: "Marion Bergnaum",
-          },
-        },
-      ],
-      contacts: [
-        {
-          id: "1",
-          fullName: "John Doe",
-          email: "john.doe@email.com",
-          contactType: "Project Officer",
-        },
-      ],
-      currentPhaseName: "Federal Comment" as const,
-    };
     render(
       <TestProvider>
-        <ApplicationWorkflow demonstration={demonstration} />
+        <ApplicationWorkflow demonstrationId={MOCK_DEMONSTRATION_ID} />
       </TestProvider>
     );
-    expect(screen.getByText("APPLICATION")).toBeInTheDocument();
+    waitFor(() => {
+      expect(screen.getByText("APPLICATION")).toBeInTheDocument();
+    });
   });
 });
--- client/src/components/application/ApplicationWorkflow.tsx
+++ client/src/components/application/ApplicationWorkflow.tsx
@@ -1,26 +1,60 @@
 import React from "react";
 import { PhaseSelector } from "./phase-selector/PhaseSelector";
 import { DemonstrationStatusBadge } from "../badge/DemonstrationStatusBadge";
-import type { Demonstration } from "demos-server";
+import type { Demonstration, PhaseName, ApplicationDate, PhaseStatus } from "demos-server";
+import { gql, useQuery } from "@apollo/client";
+import { Loading } from "components/loading/Loading";
 
+export const GET_WORKFLOW_DEMONSTRATION_QUERY = gql`
+  query GetApplicationWorkflow($id: ID!) {
+    demonstration(id: $id) {
+      id
+      status
+      currentPhaseName
+      phases {
+        phaseName
+        phaseStatus
+        phaseDates {
+          dateType
+          dateValue
+        }
+      }
+    }
+  }
+`;
+
+type SimplePhase = {
+  phaseName: PhaseName;
+  phaseStatus: PhaseStatus;
+  phaseDates: Pick<ApplicationDate, "dateType" | "dateValue">[];
+};
 export type ApplicationWorkflowDemonstration = Pick<
   Demonstration,
   "id" | "status" | "currentPhaseName"
->;
+> & {
+  phases: SimplePhase[];
+};
 
-export const ApplicationWorkflow = ({
-  demonstration,
-}: {
-  demonstration: ApplicationWorkflowDemonstration;
-}) => {
-  return (
-    <div className="flex flex-col gap-sm p-md">
-      <div className="flex w-full">
-        <h3 className="text-brand text-2xl font-bold">APPLICATION</h3>
-        <DemonstrationStatusBadge demonstrationStatus={demonstration.status} />
-      </div>
-      <hr className="text-border-rules" />
-      <PhaseSelector demonstration={demonstration} />
-    </div>
+export const ApplicationWorkflow = ({ demonstrationId }: { demonstrationId: string }) => {
+  const { data, loading, error } = useQuery<{ demonstration: ApplicationWorkflowDemonstration }>(
+    GET_WORKFLOW_DEMONSTRATION_QUERY,
+    {
+      variables: { id: demonstrationId },
+    }
   );
+
+  if (loading) return <Loading />;
+  if (error) return <p>Error Loading Application Workflow: {error.message}</p>;
+  if (data) {
+    return (
+      <div className="flex flex-col gap-sm p-md">
+        <div className="flex w-full">
+          <h3 className="text-brand text-2xl font-bold">APPLICATION</h3>
+          <DemonstrationStatusBadge demonstrationStatus={data.demonstration.status} />
+        </div>
+        <hr className="text-border-rules" />
+        <PhaseSelector demonstration={data.demonstration} />
+      </div>
+    );
+  }
 };
