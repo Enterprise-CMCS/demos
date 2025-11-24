02be1f555dd92eb9c454cc5eeaaf641d2f368ef7
Dustin <Dustin.H@globalalliantinc.com>
2025-11-24 17:57:16 +0000

    DEMOS-975: simplest possibel verison

--- client/src/components/input/date/DatePicker.tsx
+++ client/src/components/input/date/DatePicker.tsx
@@ -1,54 +1,35 @@
 import React from "react";
+import { Input } from "components/input/Input";
 
 export interface DatePickerProps
   extends Omit<React.InputHTMLAttributes<HTMLInputElement>, "type" | "onChange"> {
-  label?: string;
-  onValueChange?: (value: string) => void;
-  inputClassName?: string;
-  onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  label: string;
+  required?: boolean;
+  value?: string;
+  onValueChange?: (newDate: string) => void;
 }
 
 export const DatePicker: React.FC<DatePickerProps> = ({
   label,
-  id,
   name,
-  className = "",
-  inputClassName = "",
+  id,
   required,
-  onChange,
+  value,
   onValueChange,
 }) => {
-  const inputId = id ?? name;
-  const inputMin = "1900-01-01";
-  // Maybe candidate for a spread operator "..props" pattern.
-  // Figure we'd wait for a use case.
-
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    onChange?.(e);
     onValueChange?.(e.target.value);
   };
 
   return (
-    <div className={className}>
-      {label && (
-        <label
-          htmlFor={inputId}
-          className="block text-sm font-bold mb-1"
-        >
-          {label}
-          {required && <span className="text-red-600 ml-0.5">*</span>}
-        </label>
-      )}
-
-      <input
-        id={inputId}
-        name={name}
-        type="date"
-        onChange={handleChange}
-        className={`w-full border border-border-fields px-1 py-1 text-sm rounded ${inputClassName}`}
-        min={inputMin}
-        required={required}
-      />
-    </div>
+    <Input
+      type="date"
+      label={label}
+      name={name}
+      id={id}
+      isRequired={required ?? false}
+      value={value}
+      onChange={handleChange}
+    />
   );
 };
