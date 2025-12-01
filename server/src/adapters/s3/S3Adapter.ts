1f526693887feae04fbd7a3b84c6d133041b1f72
Tresdon Jones <tresdon.jones@icf.com>
2025-12-01 10:57:49 -0500

    Remove local

--- server/src/adapters/s3/S3Adapter.ts
+++ server/src/adapters/s3/S3Adapter.ts
@@ -1,5 +1,4 @@
 import { createAWSS3Adapter } from "./AwsS3Adapter.js";
-import { createLocalS3Adapter } from "./LocalS3Adapter.js";
 
 export interface S3Adapter {
   getPresignedUploadUrl(key: string): Promise<string>;
@@ -8,6 +7,5 @@ export interface S3Adapter {
 }
 
 export function createS3Adapter(): S3Adapter {
-  if (process.env.LOCAL_SIMPLE_UPLOAD === "true") return createLocalS3Adapter();
   return createAWSS3Adapter();
 }
