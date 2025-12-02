e67d644b8e6427d3609bd617209201078598b8bf
Tom Rehman <tom.rehman@icf.com>
2025-12-02 11:09:11 -0500

    Add scheduling for two times a day to ensure timezone compatibility.

--- server/src/sql/functions.sql
+++ server/src/sql/functions.sql
@@ -477,8 +477,11 @@ BEGIN
 END
 $$;
 
+-- Scheduled to run at 00:05 Eastern
+-- Time is in UTC, so during EDT will run at 23:05 and then 00:05
+-- During EST, will run at 00:05 and then 01:05
 SELECT cron.schedule(
     'nightly-update-federal-comment-phase-status',
-    '5 0 * * *',
+    '5 4,5 * * *',
     'CALL demos_app.update_federal_comment_phase_status();'
 );
