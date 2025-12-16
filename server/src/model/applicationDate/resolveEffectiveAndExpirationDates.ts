a33a828e12c007841e0d93b110044dd5025480a8
JeffreyM-GA <Jeffrey.M@GlobalAlliantInc.com>
2025-12-16 09:19:29 -0500

    DEMOS-1195 remove duplicated logic for effective and expiration dates (#596)
    * DEMOS-1195 remove duplicated logic for effective and expiration dates
    
    * DEMOS-1195 remove erroneous spacing change
    
    * DEMOS-1195 update for consistency
    
    ---------
    
    Co-authored-by: Jeff <JeffreyM@globalalliantinc.com>
    Co-authored-by: Jeffrey Mitchell <jeffreym@Jeffreys-MacBook-Pro.local>

---
+++ server/src/model/applicationDate/resolveEffectiveAndExpirationDates.ts
@@ -0,0 +1,49 @@
+import { TZDate } from "@date-fns/tz";
+import {
+  checkInputDateIsStartOfDay,
+  checkInputDateIsEndOfDay,
+} from "./checkInputDateFunctions.js";
+import { parseDateTimeOrLocalDateToEasternTZDate } from "../../dateUtilities.js";
+import { DateTimeOrLocalDate } from "../../types.js";
+
+type DateInput = {
+  effectiveDate?: DateTimeOrLocalDate | null;
+  expirationDate?: DateTimeOrLocalDate | null;
+};
+
+export function resolveEffectiveAndExpirationDates(
+  input: DateInput
+): {
+  effectiveDate?: TZDate | null;
+  expirationDate?: TZDate | null;
+} {
+  let effectiveDate: TZDate | null | undefined;
+  let expirationDate: TZDate | null | undefined;
+
+  if (input.effectiveDate) {
+    const parsed = parseDateTimeOrLocalDateToEasternTZDate(
+      input.effectiveDate,
+      "Start of Day"
+    );
+    checkInputDateIsStartOfDay("effectiveDate", parsed);
+    effectiveDate = parsed.easternTZDate;
+  } else if (input.effectiveDate === null) {
+    effectiveDate = null;
+  }
+
+  if (input.expirationDate) {
+    const parsed = parseDateTimeOrLocalDateToEasternTZDate(
+      input.expirationDate,
+      "End of Day"
+    );
+    checkInputDateIsEndOfDay("expirationDate", parsed);
+    expirationDate = parsed.easternTZDate;
+  } else if (input.expirationDate === null) {
+    expirationDate = null;
+  }
+
+  return {
+    ...(effectiveDate !== undefined && { effectiveDate }),
+    ...(expirationDate !== undefined && { expirationDate }),
+  };
+}
