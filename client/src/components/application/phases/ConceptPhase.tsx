9c6ae0ea6e65b7835575d2945cd2efbcb135ae10
JeffreyM-GA <Jeffrey.M@GlobalAlliantInc.com>
2025-12-19 12:26:09 -0500

    DEMOS-1235 add General Files to pre-submission document list (#612)
    Co-authored-by: Jeff <JeffreyM@globalalliantinc.com>

--- client/src/components/application/phases/ConceptPhase.tsx
+++ client/src/components/application/phases/ConceptPhase.tsx
@@ -44,7 +44,7 @@ export const getConceptPhaseComponentFromDemonstration = (
   setSelectedPhase?: (phase: PhaseName) => void
 ) => {
   const preSubmissionDocuments = demonstration.documents.filter(
-    (document) => document.documentType === "Pre-Submission"
+    (document) => document.documentType === "Pre-Submission" || document.documentType === "General File"
   );
 
   return (
