a094cb3327297761344c9b71bc7edc1e167a28ab
Connor J. Parke <connor.parke@icf.com>
2026-01-07 15:51:27 +0000

    DEMOS-1167: checkpoint, simplifying

--- server/src/model/applicationNote/validateAllowedNoteChangeByPhase.ts
+++ server/src/model/applicationNote/validateAllowedNoteChangeByPhase.ts
@@ -1,33 +1,20 @@
 import { PrismaTransactionClient } from "../../prismaClient";
 import { SetApplicationNotesInput } from "../../types";
-import { filterChangingNoteTypes } from "./filterChangingNoteTypes";
-import { queryApplicationNotesByNoteTypes } from "./queries/queryApplicationNotesByNoteTypes";
-import { queryApplicationNoteTypesOnFinishedPhases } from "./queries/queryApplicationNoteTypesOnFinishedPhases";
 
 export async function validateAllowedNoteChangeByPhase(
   tx: PrismaTransactionClient,
   input: SetApplicationNotesInput
 ): Promise<void> {
-  const existingApplicationNotes = await queryApplicationNotesByNoteTypes(
-    tx,
-    input.applicationId,
-    input.applicationNotes.map((note) => note.noteType)
-  );
+  const completedPhases = await tx.applicationPhase.findMany({
+    where: {
+      applicationId: input.applicationId,
+      phaseStatusId: "Completed",
+    },
+  });
 
-  const noteTypesToBeChanged = filterChangingNoteTypes(
-    input.applicationNotes,
-    existingApplicationNotes
-  );
-
-  const restrictedPhaseNoteTypeChanges = await queryApplicationNoteTypesOnFinishedPhases(
-    tx,
-    input.applicationId,
-    noteTypesToBeChanged
-  );
-
-  if (restrictedPhaseNoteTypeChanges.length > 0) {
-    const errors = restrictedPhaseNoteTypeChanges
-      .map((note) => `${note.noteTypeId} note on ${note.phaseId} phase`)
+  if (disallowedNotes.length > 0) {
+    const errors = disallowedNotes
+      .map((disallowedNote) => `${disallowedNote.noteType} note on ${disallowedNote.phaseId} phase`)
       .join(", ");
     throw new Error(
       `Cannot modify notes because they are associated with finished phases: ${errors}.`
