50e1cda06d5efa3baab576acaae405cd6959423b
Tom Rehman <tom.rehman@icf.com>
2026-01-15 16:24:41 -0500

    DEMOS-1365-add-date-fields-for-approval-summary (#714)
    * Updates caused by running prisma format.
    
    * Harmonizes the naming of type limiter tables and adds a new flag to denote them. Updates the README accordingly.
    
    * Initial commit to add tagging stuff for the DB. Need to test / play with this to ensure that it is working.
    
    * Renamed tag to tagConfiguration for the fields on the assignment table based on discussion with Connor. Added the baseline demo types to the migration.
    
    * Add approval summary phase dates to constants and DB.
    
    * Oops, forgot about this test when I added the new dates.

---
+++ server/src/model/migrations/20260115182039_add_approval_summary_phase_dates/migration.sql
@@ -0,0 +1,11 @@
+INSERT INTO
+    demos_app.date_type
+VALUES
+    ('Application Details Marked Complete Date'),
+    ('Application Demonstration Types Marked Complete Date');
+
+INSERT INTO
+    demos_app.phase_date_type
+VALUES
+    ('Approval Summary', 'Application Details Marked Complete Date'),
+    ('Approval Summary', 'Application Demonstration Types Marked Complete Date');
