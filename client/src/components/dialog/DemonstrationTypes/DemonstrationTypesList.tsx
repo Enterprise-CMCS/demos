aaa784fc19a3029272534b9facef0c7357a2a8da
Connor J. Parke <connor.parke@icf.com>
2026-01-16 12:29:03 -0500

    Demos 1372 apply demonstration types via modal part1 (#716)
    * DEMOS-1372: created unintegrated modal with mock data
    
    * DEMOS-1372: loosened restriction on undefined, now prepares for loading states without jumping
    
    * DEMOS-1372: added dialog context test

---
+++ client/src/components/dialog/DemonstrationTypes/DemonstrationTypesList.tsx
@@ -0,0 +1,48 @@
+import React from "react";
+import { DemonstrationType } from "./ApplyDemonstrationTypesDialog";
+import { DeleteIcon } from "components/icons";
+import { formatDate } from "util/formatDate";
+
+export const DemonstrationTypesList = ({
+  demonstrationTypes,
+  removeDemonstrationType,
+}: {
+  demonstrationTypes: DemonstrationType[];
+  removeDemonstrationType: (tag: string) => void;
+}) => {
+  return (
+    <div className="flex flex-col gap-2">
+      <p className="font-bold">Types to be added ({demonstrationTypes.length})</p>
+      {demonstrationTypes.length === 0 && <p>No demonstration types added.</p>}
+      {demonstrationTypes.length > 0 && (
+        <ul className="border border-gray-300 border-b-0">
+          {demonstrationTypes.map((type, index) => (
+            <li key={index} className="p-1 border-b border-gray-300 flex justify-between">
+              <div>
+                <p className="font-bold text-lg">{type.tag}</p>
+                <span>
+                  Effective: {formatDate(type.effectiveDate)} &bull; Expires:{" "}
+                  {formatDate(type.expirationDate)}
+                </span>
+              </div>
+              <div className="flex items-center">
+                <button
+                  className="p-1"
+                  onClick={() => removeDemonstrationType(type.tag)}
+                  name="remove-type"
+                >
+                  <DeleteIcon
+                    height="20px"
+                    width="20px"
+                    fill="currentColor"
+                    className="text-gray-700"
+                  />
+                </button>
+              </div>
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
+};
