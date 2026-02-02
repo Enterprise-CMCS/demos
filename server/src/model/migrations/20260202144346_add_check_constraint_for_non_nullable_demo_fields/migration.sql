98f5315ddb49b6367c14cea32133c4830bd34dcd
Connor J. Parke <connor.parke@icf.com>
2026-02-02 15:23:15 +0000

    DEMOS-1289: format sql

--- server/src/model/migrations/20260202144346_add_check_constraint_for_non_nullable_demo_fields/migration.sql
+++ server/src/model/migrations/20260202144346_add_check_constraint_for_non_nullable_demo_fields/migration.sql
@@ -1,9 +1,10 @@
 ALTER TABLE demos_app.demonstration ADD CONSTRAINT check_demonstration_non_null_fields_when_approved CHECK (
   NOT (
-    status_id = 'Approved' AND (
-      effective_date IS NULL 
-      OR expiration_date IS NULL 
-      OR sdg_division_id IS NULL 
+    status_id = 'Approved'
+    AND (
+      effective_date IS NULL
+      OR expiration_date IS NULL
+      OR sdg_division_id IS NULL
       OR signature_level_id IS NULL
     )
   )
@@ -11,12 +12,14 @@ ALTER TABLE demos_app.demonstration ADD CONSTRAINT check_demonstration_non_null_
 
 ALTER TABLE demos_app.amendment ADD CONSTRAINT check_amendment_non_null_effective_date_when_approved CHECK (
   NOT (
-    status_id = 'Approved' AND effective_date IS NULL 
+    status_id = 'Approved'
+    AND effective_date IS NULL
   )
 );
 
 ALTER TABLE demos_app.extension ADD CONSTRAINT check_extension_non_null_effective_date_when_approved CHECK (
   NOT (
-    status_id = 'Approved' AND effective_date IS NULL 
+    status_id = 'Approved'
+    AND effective_date IS NULL
   )
 );
