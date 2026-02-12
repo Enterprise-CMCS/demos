4be0f0c79cfbe1f5a49e9b296c7e1adabd6c1b37
Connor J. Parke <connor.parke@icf.com>
2026-02-12 19:40:36 +0000

    DEMOS-1472: made history non-nullable, updated diagram

---
+++ server/src/model/migrations/20260212193653_make_history_clearance_non_nullable/migration.sql
@@ -0,0 +1,16 @@
+/*
+  Warnings:
+
+  - Made the column `clearance_level_id` on table `amendment_history` required. This step will fail if there are existing NULL values in that column.
+  - Made the column `signature_level_id` on table `demonstration_history` required. This step will fail if there are existing NULL values in that column.
+  - Made the column `signature_level_id` on table `extension_history` required. This step will fail if there are existing NULL values in that column.
+
+*/
+-- AlterTable
+ALTER TABLE "amendment_history" ALTER COLUMN "clearance_level_id" SET NOT NULL;
+
+-- AlterTable
+ALTER TABLE "demonstration_history" ALTER COLUMN "signature_level_id" SET NOT NULL;
+
+-- AlterTable
+ALTER TABLE "extension_history" ALTER COLUMN "signature_level_id" SET NOT NULL;
