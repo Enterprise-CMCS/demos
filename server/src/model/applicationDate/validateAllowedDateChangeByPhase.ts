6504f925949fc5565b553b3eee358a7f9394aec7
Tom Rehman <tom.rehman@icf.com>
2026-02-27 12:30:45 -0500

    Allows the special case of editing the Expected Approval Date after the SDG Preparation phase is completed.

--- server/src/model/applicationDate/validateAllowedDateChangeByPhase.ts
+++ server/src/model/applicationDate/validateAllowedDateChangeByPhase.ts
@@ -11,11 +11,35 @@ export async function validateAllowedDateChangeByPhase(
     tx,
     input.applicationId
   );
-  const disallowedDateInputs: PhaseDateType[] = await getPhaseDateTypesByIds(
+  let disallowedDateInputs: PhaseDateType[] = await getPhaseDateTypesByIds(
     tx,
     completedPhaseIds,
     input.applicationDates.map((applicationDate) => applicationDate.dateType)
   );
+
+  // Special case: Expected Approval Date is allowed to be edited after SDG Preparation completed
+  // It cannot be deleted after SDG Preparation is completed
+  const disallowedContainsExpectedApprovalDate = disallowedDateInputs.some(
+    (phaseDateType) => phaseDateType.dateTypeId === "Expected Approval Date"
+  );
+
+  // If we see it in disallowed, check if it is a deletion or an edit
+  // If deletion, we'll catch the special case, otherwise, we remove it as it is allowed
+  if (disallowedContainsExpectedApprovalDate) {
+    const disallowedOperationIsDeletion = input.applicationDates.some(
+      (applicationDate) =>
+        applicationDate.dateType === "Expected Approval Date" && applicationDate.dateValue === null
+    );
+
+    if (disallowedOperationIsDeletion) {
+      throw new Error(
+        "You cannot delete the Expected Approval Date after the SDG Preparation phase is completed."
+      );
+    }
+    disallowedDateInputs = disallowedDateInputs.filter(
+      (phaseDateType) => phaseDateType.dateTypeId !== "Expected Approval Date"
+    );
+  }
 
   if (disallowedDateInputs.length > 0) {
     const errorMessages = disallowedDateInputs.map(
