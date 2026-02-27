93ee268a53e1b196858757220a7b8ca46a24bdc9
Tresdon Jones <tresdon.jones@icf.com>
2026-02-27 12:46:38 -0500

    Add modification workflows

--- client/src/pages/DemonstrationDetail/modifications/ModificationTabSideNav.tsx
+++ client/src/pages/DemonstrationDetail/modifications/ModificationTabSideNav.tsx
@@ -3,11 +3,22 @@ import React from "react";
 import { ModificationItem } from "./ModificationTabs";
 import { DetailsIcon, ListIcon, OpenFolderIcon } from "components/icons";
 import { ModificationDetailsSummary } from "./ModificationDetailsSummary";
+import { AmendmentWorkflow, ExtensionWorkflow } from "components/application";
 
 const TABS = {
   APPLICATION: "application",
   DETAILS: "details",
   DOCUMENTS: "documents",
+};
+
+const ModificationWorkflow = ({ modificationItem }: { modificationItem: ModificationItem }) => {
+  if (modificationItem.modificationType === "amendment") {
+    return <AmendmentWorkflow />;
+  } else if (modificationItem.modificationType === "extension") {
+    return <ExtensionWorkflow />;
+  } else {
+    return <div>Unsupported modification type! {modificationItem.modificationType}</div>;
+  }
 };
 
 export const ModificationTabSideNav = ({
@@ -18,7 +29,7 @@ export const ModificationTabSideNav = ({
   return (
     <VerticalTabs defaultValue={TABS.APPLICATION}>
       <Tab icon={<ListIcon />} value={TABS.APPLICATION} label="Application">
-        Application Tab for {modificationItem.name}
+        <ModificationWorkflow modificationItem={modificationItem} />
       </Tab>
       <Tab icon={<DetailsIcon />} value={TABS.DETAILS} label="Details">
         <ModificationDetailsSummary modificationItem={modificationItem} />
