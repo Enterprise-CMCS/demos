eceb8bc9069d4b49807c76ac6053e077696bacf3
Lucas Nunes <nunesmlucasg@gmail.com>
2026-03-02 12:04:28 -0500

    DEMOS-1606: Add Federal Comment Internal Analysis Document Type (#902)
    DEMOS-1606
    
    Co-authored-by: Lucas Nunes <lucas.nunes@icf.com>

--- client/src/components/dialog/document/phases/FederalCommentUploadDialog.tsx
+++ client/src/components/dialog/document/phases/FederalCommentUploadDialog.tsx
@@ -3,7 +3,10 @@ import React from "react";
 import { AddDocumentDialog } from "components/dialog/document";
 import { DocumentType } from "demos-server";
 
-const DOCUMENT_TYPE_SUBSET: DocumentType[] = ["General File"];
+const DOCUMENT_TYPE_SUBSET: DocumentType[] = [
+  "General File",
+  "Federal Comment Internal Analysis Document",
+];
 
 const REFETCH_QUERIES = ["GetConceptDocuments", "GetDemonstrationDocuments"];
 
@@ -18,7 +21,7 @@ export const FederalCommentUploadDialog: React.FC<Props> = ({ onClose, applicati
       onClose={onClose}
       documentTypeSubset={DOCUMENT_TYPE_SUBSET}
       applicationId={applicationId}
-      titleOverride="Internal Analysis Document"
+      titleOverride="Add Federal Comment Document"
       refetchQueries={REFETCH_QUERIES}
       phaseName="Federal Comment"
     />
