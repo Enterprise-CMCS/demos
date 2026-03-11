b0e61831ce543b0f54eaf969d7c08aa0ed163f9a
Tom Rehman <tom.rehman@icf.com>
2026-03-11 10:48:57 -0400

    Adding testing users to the lower environments, and supporting db resets in IMPL.

--- deployment/databaseRoles.ts
+++ deployment/databaseRoles.ts
@@ -24,6 +24,16 @@ export const dev: Role[] = [
     memberships: ["demos_read", "demos_write", "demos_delete"],
     systemRole: true,
   },
+  {
+    name: "jchilukuri",
+    memberships: ["demos_read", "demos_write", "demos_delete"],
+    systemRole: false,
+  },
+  {
+    name: "pgurban",
+    memberships: ["demos_read", "demos_write", "demos_delete"],
+    systemRole: false,
+  },
 ];
 
 export const test: Role[] = [
@@ -42,6 +52,16 @@ export const test: Role[] = [
     memberships: ["demos_read", "demos_write", "demos_delete"],
     systemRole: true,
   },
+  {
+    name: "jchilukuri",
+    memberships: ["demos_read", "demos_write", "demos_delete"],
+    systemRole: false,
+  },
+  {
+    name: "pgurban",
+    memberships: ["demos_read", "demos_write", "demos_delete"],
+    systemRole: false,
+  },
 ];
 
 export const impl: Role[] = [
@@ -59,6 +79,16 @@ export const impl: Role[] = [
     name: "demos_delete_infected_file",
     memberships: ["demos_read", "demos_write", "demos_delete"],
     systemRole: true,
+  },
+  {
+    name: "jchilukuri",
+    memberships: ["demos_read", "demos_write", "demos_delete"],
+    systemRole: false,
+  },
+  {
+    name: "pgurban",
+    memberships: ["demos_read", "demos_write", "demos_delete"],
+    systemRole: false,
   },
 ];
 
