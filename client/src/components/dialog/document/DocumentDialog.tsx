83ff3bcc55015572ebc6f85c2f090ed0459e91ec
Dustin Horning <dustin.h@globalalliantinc.com>
2026-03-12 10:44:01 -0400

    Demos-1282 Allow for xlsm files PR 1 / 2 (#929)
    * DEMOS-1282: fixed doc upload to work with XLSM
    
    * DEMOS-1282: added XLSM file type
    
    * DEMOS-1282: cleaned up the doc types list
    
    * DEMOS-1282: had to tighten up the constant list
    
    * DEMOS-1282: put back the extensions
    
    * DEMOS-1282: added back to mime type since can be inconsistent
    
    With MS files
    
    * DEMOS-1282: added both BN workbooks together
    
    Final BN Worksheet and Final Budget Neutrality Formulation Workbook
    should both live until told otherwise
    
    * DEMOS-1282: pulled this out of phase 7
    
    * DEMOS-1282: Removed BN from phase 7

--- client/src/components/dialog/document/DocumentDialog.tsx
+++ client/src/components/dialog/document/DocumentDialog.tsx
@@ -38,10 +38,18 @@ const ALLOWED_MIME_TYPES = [
   "application/vnd.ms-excel",
   "application/vnd.ms-excel.sheet.macroEnabled.12",
   "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
-  "application/zip",
 ];
 
-const ACCEPTED_EXTENSIONS = ".pdf,.doc,.docx,.xls,.xlsx,.zip";
+const ALLOWED_FILE_EXTENSIONS = [
+  ".pdf",
+  ".doc",
+  ".docx",
+  ".xls",
+  ".xlsx",
+  ".xlsm",
+];
+
+const ACCEPTED_EXTENSIONS = ALLOWED_FILE_EXTENSIONS.join(",");
 const MAX_FILE_SIZE_MB = 600;
 const MAX_FILE_SIZE_BYTES = MAX_FILE_SIZE_MB * 1024 * 1024;
 const MAX_FILENAME_DISPLAY_LENGTH = 60;
@@ -334,6 +342,7 @@ export const DocumentDialog: React.FC<DocumentDialogProps> = ({
 
   const { file, uploadProgress, uploadStatus, handleFileChange, setFile } = useFileUpload({
     allowedMimeTypes: ALLOWED_MIME_TYPES,
+    allowedFileExtensions: ALLOWED_FILE_EXTENSIONS,
     maxFileSizeBytes: MAX_FILE_SIZE_BYTES,
     onErrorCallback: (msg: ErrorMessage) => showError(msg),
   });
