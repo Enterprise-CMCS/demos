a98b50038a301d6a68e252dccda2a5e2eff77032
Connor J. Parke <connor.parke@icf.com>
2026-03-12 15:26:10 +0000

    DEMOS-1719: added comment explaining DB constraint casting

--- server/src/model/tag/getTagListByTagType.ts
+++ server/src/model/tag/getTagListByTagType.ts
@@ -5,6 +5,7 @@ export async function getTagListByTagType(tagTypeId: TagType): Promise<Tag[]> {
   const result = await getTagByTagType(tagTypeId);
   return result.map((tag) => ({
     tagName: tag.tagNameId,
+    // casting enforced by database constraints
     approvalStatus: tag.statusId as TagStatus,
   }));
 }
