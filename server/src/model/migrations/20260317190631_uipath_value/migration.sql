e54c9b6b996a7c3a46e3dee83aa39f269da262a6
Dustin <Dustin.H@globalalliantinc.com>
2026-03-17 19:36:15 +0000

    DEMOS-1685: Adjusted some prisma models for uipath

---
+++ server/src/model/migrations/20260317190631_uipath_value/migration.sql
@@ -0,0 +1,50 @@
+-- DropForeignKey
+ALTER TABLE "amendment" DROP CONSTRAINT "amendment_id_application_type_id_fkey";
+
+-- DropForeignKey
+ALTER TABLE "demonstration" DROP CONSTRAINT "demonstration_id_application_type_id_fkey";
+
+-- DropForeignKey
+ALTER TABLE "extension" DROP CONSTRAINT "extension_id_application_type_id_fkey";
+
+-- CreateTable
+CREATE TABLE "uipath_value" (
+    "id" UUID NOT NULL,
+    "uipath_result_id" UUID NOT NULL,
+    "document_id" UUID NOT NULL,
+    "application_id" UUID NOT NULL,
+    "field_id" TEXT NOT NULL,
+    "value" TEXT NOT NULL,
+    "text_length" INTEGER NOT NULL,
+    "text_start_index" INTEGER NOT NULL,
+    "confidence" DOUBLE PRECISION NOT NULL,
+    "token_list" JSONB NOT NULL,
+    "created_at" TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMPTZ NOT NULL,
+
+    CONSTRAINT "uipath_value_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE INDEX "uipath_value_uipath_result_id_document_id_application_id_idx" ON "uipath_value"("uipath_result_id", "document_id", "application_id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "uipath_value_id_application_id_field_id_value_key" ON "uipath_value"("id", "application_id", "field_id", "value");
+
+-- AddForeignKey
+ALTER TABLE "amendment" ADD CONSTRAINT "amendment_id_application_type_id_fkey" FOREIGN KEY ("id", "application_type_id") REFERENCES "application"("id", "application_type_id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "demonstration" ADD CONSTRAINT "demonstration_id_application_type_id_fkey" FOREIGN KEY ("id", "application_type_id") REFERENCES "application"("id", "application_type_id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "extension" ADD CONSTRAINT "extension_id_application_type_id_fkey" FOREIGN KEY ("id", "application_type_id") REFERENCES "application"("id", "application_type_id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "uipath_value" ADD CONSTRAINT "uipath_value_uipath_result_id_fkey" FOREIGN KEY ("uipath_result_id") REFERENCES "uipath_result"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "uipath_value" ADD CONSTRAINT "uipath_value_application_id_fkey" FOREIGN KEY ("application_id") REFERENCES "application"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "uipath_value" ADD CONSTRAINT "uipath_value_document_id_fkey" FOREIGN KEY ("document_id") REFERENCES "document"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
