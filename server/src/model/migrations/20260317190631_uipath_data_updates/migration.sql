fc112531e26f7a421e87ffe95d7a7ad0faab21e6
Dustin <Dustin.H@globalalliantinc.com>
2026-03-20 16:29:27 +0000

    DEMOS-1605: moved all migrations into one file

---
+++ server/src/model/migrations/20260317190631_uipath_data_updates/migration.sql
@@ -0,0 +1,210 @@
+SET search_path TO demos_app;
+
+DROP TABLE IF EXISTS "uipath_result_field";
+
+-- CreateTable
+CREATE TABLE "uipath_value" (
+    "id" UUID NOT NULL,
+    "uipath_result_id" UUID NOT NULL,
+    "document_id" UUID NOT NULL,
+    "application_id" UUID NOT NULL,
+    "field_id" TEXT NOT NULL,
+    "value" TEXT NOT NULL,
+    "text_length" INTEGER NOT NULL,
+    "text_start_index" INTEGER NOT NULL,
+    "confidence" DOUBLE PRECISION NOT NULL,
+    "token_list" JSON NOT NULL,
+    "created_at" TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMPTZ NOT NULL,
+
+    CONSTRAINT "uipath_value_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE INDEX "uipath_value_uipath_result_id_document_id_application_id_idx" ON "uipath_value"("uipath_result_id", "document_id", "application_id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "uipath_value_id_application_id_field_id_value_key" ON "uipath_value"("id", "application_id", "field_id", "value");
+
+-- AddForeignKey
+ALTER TABLE "uipath_value" ADD CONSTRAINT "uipath_value_uipath_result_id_fkey" FOREIGN KEY ("uipath_result_id") REFERENCES "uipath_result"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "uipath_value" ADD CONSTRAINT "uipath_value_application_id_fkey" FOREIGN KEY ("application_id") REFERENCES "application"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "uipath_value" ADD CONSTRAINT "uipath_value_document_id_fkey" FOREIGN KEY ("document_id") REFERENCES "document"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- CreateTable
+CREATE TABLE IF NOT EXISTS "application_tag_suggestion_status" (
+    "id" TEXT NOT NULL,
+
+    CONSTRAINT "application_tag_suggestion_status_pkey" PRIMARY KEY ("id")
+);
+
+-- Seed default values
+INSERT INTO "application_tag_suggestion_status" ("id")
+VALUES
+  ('Pending'),
+  ('Accepted'),
+  ('Replaced'),
+  ('Removed')
+;
+
+-- application_tag_suggestion_extract_field_limit
+
+-- CreateTable
+CREATE TABLE IF NOT EXISTS "application_tag_suggestion_extract_field_limit" (
+    "id" TEXT NOT NULL,
+
+    CONSTRAINT "application_tag_suggestion_extract_field_limit_pkey" PRIMARY KEY ("id")
+);
+
+-- Seed default values
+INSERT INTO
+    "application_tag_suggestion_extract_field_limit"
+VALUES
+  ('demo_type');
+
+-- CreateTable
+CREATE TABLE "uipath_result_history" (
+    "revision_id" SERIAL NOT NULL,
+    "revision_type" "revision_type_enum" NOT NULL,
+    "modified_at" TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "id" UUID NOT NULL,
+    "request_id" TEXT NOT NULL,
+    "response" JSON NOT NULL,
+    "project_id" TEXT NOT NULL,
+    "document_id" UUID NOT NULL,
+    "application_id" UUID NOT NULL,
+    "status_id" TEXT NOT NULL,
+    "created_at" TIMESTAMPTZ NOT NULL,
+    "updated_at" TIMESTAMPTZ NOT NULL,
+
+    CONSTRAINT "uipath_result_history_pkey" PRIMARY KEY ("revision_id")
+);
+
+-- CreateTable
+CREATE TABLE "uipath_value_history" (
+    "revision_id" SERIAL NOT NULL,
+    "revision_type" "revision_type_enum" NOT NULL,
+    "modified_at" TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "id" UUID NOT NULL,
+    "uipath_result_id" UUID NOT NULL,
+    "document_id" UUID NOT NULL,
+    "application_id" UUID NOT NULL,
+    "field_id" TEXT NOT NULL,
+    "value" TEXT NOT NULL,
+    "text_length" INTEGER NOT NULL,
+    "text_start_index" INTEGER NOT NULL,
+    "confidence" DOUBLE PRECISION NOT NULL,
+    "token_list" JSON NOT NULL,
+    "created_at" TIMESTAMPTZ NOT NULL,
+    "updated_at" TIMESTAMPTZ NOT NULL,
+
+    CONSTRAINT "uipath_value_history_pkey" PRIMARY KEY ("revision_id")
+);
+
+-- CreateTable
+CREATE TABLE "application_tag_suggestion_history" (
+    "revision_id" SERIAL NOT NULL,
+    "revision_type" "revision_type_enum" NOT NULL,
+    "modified_at" TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "id" UUID NOT NULL,
+    "application_id" UUID NOT NULL,
+    "value" TEXT NOT NULL,
+    "status_id" TEXT NOT NULL,
+    "created_at" TIMESTAMPTZ NOT NULL,
+    "updated_at" TIMESTAMPTZ NOT NULL,
+
+    CONSTRAINT "application_tag_suggestion_history_pkey" PRIMARY KEY ("revision_id")
+);
+
+-- CreateTable
+CREATE TABLE "application_tag_suggestion_extract_history" (
+    "revision_id" SERIAL NOT NULL,
+    "revision_type" "revision_type_enum" NOT NULL,
+    "modified_at" TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "suggestion_id" UUID NOT NULL,
+    "uipath_value_id" UUID NOT NULL,
+    "application_id" UUID NOT NULL,
+    "field_id" TEXT NOT NULL,
+    "value" TEXT NOT NULL,
+    "start_page_no" INTEGER NOT NULL,
+    "end_page_no" INTEGER NOT NULL,
+    "created_at" TIMESTAMPTZ NOT NULL,
+    "updated_at" TIMESTAMPTZ NOT NULL,
+
+    CONSTRAINT "application_tag_suggestion_extract_history_pkey" PRIMARY KEY ("revision_id")
+);
+
+-- CreateTable
+CREATE TABLE "application_tag_suggestion" (
+    "id" UUID NOT NULL,
+    "application_id" UUID NOT NULL,
+    "value" TEXT NOT NULL,
+    "status_id" TEXT NOT NULL,
+    "created_at" TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMPTZ NOT NULL,
+
+    CONSTRAINT "application_tag_suggestion_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "application_tag_suggestion_extract" (
+    "suggestion_id" UUID NOT NULL,
+    "uipath_value_id" UUID NOT NULL,
+    "application_id" UUID NOT NULL,
+    "field_id" TEXT NOT NULL,
+    "value" TEXT NOT NULL,
+    "start_page_no" INTEGER NOT NULL,
+    "end_page_no" INTEGER NOT NULL,
+    "created_at" TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMPTZ NOT NULL,
+
+    CONSTRAINT "application_tag_suggestion_extract_pkey" PRIMARY KEY ("suggestion_id", "uipath_value_id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "application_tag_suggestion_id_application_id_value_key" ON "application_tag_suggestion"("id", "application_id", "value");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "uipath_value_id_application_id_value_key" ON "uipath_value"("id", "application_id", "value");
+
+-- AddForeignKey
+ALTER TABLE "application_tag_suggestion" ADD CONSTRAINT "application_tag_suggestion_status_id_fkey" FOREIGN KEY ("status_id") REFERENCES "application_tag_suggestion_status"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "application_tag_suggestion_extract" ADD CONSTRAINT "application_tag_suggestion_extract_field_id_fkey" FOREIGN KEY ("field_id") REFERENCES "application_tag_suggestion_extract_field_limit"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "application_tag_suggestion_extract" ADD CONSTRAINT "application_tag_suggestion_extract_uipath_value_id_application_id_value_fkey" FOREIGN KEY ("uipath_value_id", "application_id", "value") REFERENCES "uipath_value"("id", "application_id", "value") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "application_tag_suggestion_extract" ADD CONSTRAINT "application_tag_suggestion_extract_suggestion_id_application_id_value_fkey" FOREIGN KEY ("suggestion_id", "application_id", "value") REFERENCES "application_tag_suggestion"("id", "application_id", "value") ON DELETE RESTRICT ON UPDATE CASCADE DEFERRABLE INITIALLY DEFERRED;
+
+-- DropForeignKey
+ALTER TABLE "uipath_result" DROP CONSTRAINT "uipath_result_document_id_fkey";
+
+-- AlterTable
+ALTER TABLE "uipath_result" ADD COLUMN     "application_id" UUID NOT NULL,
+ADD COLUMN     "updated_at" TIMESTAMPTZ NOT NULL,
+ALTER COLUMN "document_id" SET NOT NULL;
+
+-- CreateIndex
+CREATE UNIQUE INDEX "document_id_application_id_key" ON "document"("id", "application_id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "uipath_result_document_id_key" ON "uipath_result"("document_id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "uipath_result_id_document_id_application_id_key" ON "uipath_result"("id", "document_id", "application_id");
+
+-- RenameForeignKey
+ALTER TABLE "application_tag_suggestion_extract" RENAME CONSTRAINT "application_tag_suggestion_extract_suggestion_id_application_id" TO "application_tag_suggestion_extract_suggestion_id_applicati_fkey";
+
+-- RenameForeignKey
+ALTER TABLE "application_tag_suggestion_extract" RENAME CONSTRAINT "application_tag_suggestion_extract_uipath_value_id_application_" TO "application_tag_suggestion_extract_uipath_value_id_applica_fkey";
+
+-- AddForeignKey
+ALTER TABLE "uipath_result" ADD CONSTRAINT "uipath_result_document_id_application_id_fkey" FOREIGN KEY ("document_id", "application_id") REFERENCES "document"("id", "application_id") ON DELETE RESTRICT ON UPDATE CASCADE;
