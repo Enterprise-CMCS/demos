cdf33a670c9cb5cee3ccaabfc7453bba4508e8b7
Tresdon Jones <tresdon.jones@icf.com>
2026-03-23 16:55:43 -0400

    lint

--- client/src/components/application/phases/FederalCommentPhase.tsx
+++ client/src/components/application/phases/FederalCommentPhase.tsx
@@ -1,4 +1,4 @@
-import React, { useMemo, useState } from "react";
+import React, { useState } from "react";
 import { tw } from "tags/tw";
 import { ApplicationUploadSection } from "components/application/phases/sections";
 import { formatDate } from "util/formatDate";
