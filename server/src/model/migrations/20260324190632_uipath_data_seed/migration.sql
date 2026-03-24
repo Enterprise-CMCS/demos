253ad52abebf9eb06de1c7ec855822dd758516d9
Dustin <Dustin.H@globalalliantinc.com>
2026-03-24 17:27:12 +0000

    DEMOS-1685: added deferred FK thing for application_tag_suggestion_extract

--- server/src/model/migrations/20260324190632_uipath_data_seed/migration.sql
+++ server/src/model/migrations/20260324190632_uipath_data_seed/migration.sql
@@ -12,3 +12,14 @@ VALUES
 INSERT INTO "application_tag_suggestion_extract_field_limit" ("id")
 VALUES
   ('demo_type');
+
+
+-- DeferredForeignKey
+ALTER TABLE "application_tag_suggestion_extract"
+DROP CONSTRAINT "application_tag_suggestion_extract_suggestion_id_applicati_fkey";
+
+ALTER TABLE "application_tag_suggestion_extract"
+ADD CONSTRAINT "application_tag_suggestion_extract_suggestion_id_applicati_fkey"
+FOREIGN KEY ("suggestion_id", "application_id", "value") REFERENCES "application_tag_suggestion"("id", "application_id", "value")
+ON DELETE RESTRICT ON UPDATE CASCADE
+DEFERRABLE INITIALLY DEFERRED;
