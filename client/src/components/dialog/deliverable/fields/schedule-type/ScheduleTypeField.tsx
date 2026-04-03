00045fbf7ef85df05d86268651297c1028d6af4f
Tresdon Jones <tresdon.jones@icf.com>
2026-04-03 16:31:45 -0400

    one more

--- client/src/components/dialog/deliverable/fields/schedule-type/ScheduleTypeField.tsx
+++ client/src/components/dialog/deliverable/fields/schedule-type/ScheduleTypeField.tsx
@@ -5,19 +5,6 @@ import { AutoCompleteSelect } from "components/input/select/AutoCompleteSelect";
 export const ALL_SCHEDULE_TYPES: string[] = ["Single", "Quarterly"];
 export type ScheduleType = (typeof ALL_SCHEDULE_TYPES)[number];
 
-export interface QuarterlyDeliverableSchedule {
-  q1DueDate: string;
-  q2DueDate: string;
-  q3DueDate: string;
-  q4DueDate: string;
-}
-
-export interface SingleDeliverableSchedule {
-  dueDate: string;
-}
-
-export type DeliverableSchedule = QuarterlyDeliverableSchedule | SingleDeliverableSchedule;
-
 export const ScheduleTypeField = ({
   value,
   onSelect,
