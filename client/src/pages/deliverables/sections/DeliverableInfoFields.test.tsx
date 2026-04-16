b01dcbebfa2b87c3ffa66f814fbfa16140049616
Tresdon Jones <tresdon.jones@icf.com>
2026-04-16 16:10:05 -0400

    (2/3) DEMOS-1603: Add Sections for `DeliverableDetailsManagementPage` (#1091)
    * Initial Commit
    
    * Add sections
    
    * Add tests
    
    * Update main page tests
    
    * layout updates
    
    * Add tabs
    
    * Fix tests

---
+++ client/src/pages/deliverables/sections/DeliverableInfoFields.test.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+import { render, screen } from "@testing-library/react";
+import { MOCK_DELIVERABLE_1 } from "mock-data/deliverableMocks";
+import { DeliverableInfoFields } from "./DeliverableInfoFields";
+
+describe("DeliverableInfoFields", () => {
+  it("renders the deliverable type", () => {
+    render(<DeliverableInfoFields deliverable={MOCK_DELIVERABLE_1} />);
+    expect(screen.getByTestId("deliverable-Deliverable Type")).toHaveTextContent(MOCK_DELIVERABLE_1.deliverableType);
+  });
+
+  it("renders the due date", () => {
+    render(<DeliverableInfoFields deliverable={MOCK_DELIVERABLE_1} />);
+    expect(screen.getByTestId("deliverable-Due Date")).toBeInTheDocument();
+  });
+
+  it("renders the status", () => {
+    render(<DeliverableInfoFields deliverable={MOCK_DELIVERABLE_1} />);
+    expect(screen.getByTestId("deliverable-Status")).toHaveTextContent(MOCK_DELIVERABLE_1.status);
+  });
+
+  it("renders the submission date placeholder", () => {
+    render(<DeliverableInfoFields deliverable={MOCK_DELIVERABLE_1} />);
+    expect(screen.getByTestId("deliverable-Submission Date")).toHaveTextContent("—");
+  });
+});
