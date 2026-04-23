f075e934da3e2460b33c35273415b76d86a307a6
Lucas Nunes <lucas.nunes@icf.com>
2026-04-23 11:02:20 -0400

    DEMOS-1573

--- client/src/pages/deliverables/sections/DeliverableButtons.tsx
+++ client/src/pages/deliverables/sections/DeliverableButtons.tsx
@@ -1,15 +1,36 @@
 import React from "react";
 import { SecondaryButton, TertiaryButton } from "components/button";
+import { canRequestExtension } from "components/dialog/deliverable";
+import { useDialog } from "components/dialog/DialogContext";
+import { DeliverableDetailsManagementDeliverable } from "../DeliverableDetailsManagementPage";
 
 export const DELIVERABLE_BUTTONS_NAME = "deliverable-buttons";
 export const REFERENCES_BUTTON_NAME = "button-references";
 export const REQUEST_EXTENSION_BUTTON_NAME = "button-request-extension";
 
-export const DeliverableButtons = () => {
+export const DeliverableButtons = ({
+  deliverable,
+}: {
+  deliverable: DeliverableDetailsManagementDeliverable;
+}) => {
+  const { showRequestExtensionDeliverableDialog } = useDialog();
+
+  const handleRequestExtension = () => {
+    showRequestExtensionDeliverableDialog({
+      id: deliverable.id,
+      dueDate: deliverable.dueDate,
+      demonstration: { expirationDate: deliverable.demonstration.expirationDate },
+    });
+  };
+
   return (
     <div className="flex gap-2" data-testid={DELIVERABLE_BUTTONS_NAME}>
       <TertiaryButton name={REFERENCES_BUTTON_NAME}>References</TertiaryButton>
-      <SecondaryButton name={REQUEST_EXTENSION_BUTTON_NAME}>Request Extension</SecondaryButton>
+      {canRequestExtension(deliverable.status) && (
+        <SecondaryButton name={REQUEST_EXTENSION_BUTTON_NAME} onClick={handleRequestExtension}>
+          Request Extension
+        </SecondaryButton>
+      )}
     </div>
   );
 };
