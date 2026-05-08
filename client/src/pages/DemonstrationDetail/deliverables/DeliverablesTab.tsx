305a8c3670d63521c8ccc49c83500c0e9b5b8d68
Dustin Horning <dustin.h@globalalliantinc.com>
2026-05-07 23:59:42 -0400

    Demos 2078: fixed issue where state user code was over engineered (#1209)
    * DEMOS-2078: adjusted demo delvrbles to match all/my
    
    * DEMOS-2078: fixed linting issue
    
    * DEMOS-2078: fixed up the Demo Deliverable table
    
    * DEMOS-2078: fixed over correction to state user
    
    * DEMOS-2078: my code got nicked, had to get it back

--- client/src/pages/DemonstrationDetail/deliverables/DeliverablesTab.tsx
+++ client/src/pages/DemonstrationDetail/deliverables/DeliverablesTab.tsx
@@ -9,37 +9,12 @@ import { getCurrentUser } from "components/user/UserContext";
 import type { UserType } from "demos-server";
 import {
   DELIVERABLES_PAGE_QUERY,
-  STATE_USER_DELIVERABLES_PAGE_QUERY,
   type DeliverablesQueryResult,
-  type StateUserDeliverablesQueryResult,
 } from "components/table/tables/DeliverableTable";
 import { useQuery } from "@apollo/client";
 import { useNavigate } from "react-router-dom";
 
 export const ADD_DELIVERABLE_SLOT_BUTTON_NAME = "button-add-deliverable-slot";
-const STATE_POINT_OF_CONTACT_ROLE = "State Point of Contact";
-
-type DeliverablesTabQueryResult = Partial<
-  DeliverablesQueryResult & StateUserDeliverablesQueryResult
->;
-
-const getStateUserDemonstrationDeliverables = (
-  data: DeliverablesTabQueryResult | undefined,
-  demonstrationId: string
-): DeliverablesQueryResult["deliverables"] => {
-  const deliverables =
-    data?.currentUser?.person.roles
-      .filter(
-        (roleAssignment) =>
-          roleAssignment.role === STATE_POINT_OF_CONTACT_ROLE &&
-          roleAssignment.demonstration.id === demonstrationId
-      )
-      .flatMap((roleAssignment) => roleAssignment.demonstration.deliverables) ?? [];
-
-  return Array.from(
-    new Map(deliverables.map((deliverable) => [deliverable.id, deliverable])).values()
-  );
-};
 
 export const DeliverablesTab = ({
   parentDemonstration,
@@ -49,16 +24,11 @@ export const DeliverablesTab = ({
   const { showAddDeliverableSlotDialog } = useDialog();
   const rawPersonType = getCurrentUser().currentUser?.person.personType;
   const viewMode = rawPersonType as UserType;
-  const isStateUser = rawPersonType === "demos-state-user";
   const navigate = useNavigate();
-  const { data, loading, error } = useQuery<DeliverablesTabQueryResult>(
-    isStateUser ? STATE_USER_DELIVERABLES_PAGE_QUERY : DELIVERABLES_PAGE_QUERY
-  );
-  const deliverables = isStateUser
-    ? getStateUserDemonstrationDeliverables(data, parentDemonstration.id)
-    : (data?.deliverables?.filter(
-      (deliverable) => deliverable.demonstration.id === parentDemonstration.id
-    ) ?? []);
+  const { data, loading, error } = useQuery<DeliverablesQueryResult>(DELIVERABLES_PAGE_QUERY);
+  const deliverables = data?.deliverables.filter(
+    (deliverable) => deliverable.demonstration.id === parentDemonstration.id
+  ) ?? [];
 
   return (
     <div className="flex flex-col">
@@ -73,7 +43,9 @@ export const DeliverablesTab = ({
         </IconButton>
       </TabHeader>
       {loading && <div className="p-4">Loading deliverables...</div>}
-      {error && <div className="p-4 text-red-500">Error loading deliverables.</div>}
+      {error && (
+        <div className="p-4 text-red-500">Error loading deliverables.</div>
+      )}
       {!loading && !error && (
         <DemonstrationDeliverableTable
           deliverables={deliverables}
