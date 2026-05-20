e67644ba9929b3f0aade064f97e6940e296b9faa
Connor J. Parke <connor.parke@icf.com>
2026-05-20 15:42:31 +0000

    DEMOS-1991: moved permission removal to fresh migration

---
+++ server/src/model/migrations/20260520153709_remove_unneeded_permissions/migration.sql
@@ -0,0 +1,57 @@
+SET search_path TO demos_app;
+
+DELETE FROM 
+  demos_app.role_permission
+WHERE
+  permission_id IN (
+    'View All ApplicationPhases',
+    'View ApplicationPhases on Assigned Demonstrations',
+    'View All ApplicationDates',
+    'View ApplicationDates on Assigned Demonstrations',
+    'View All ApplicationNotes',
+    'View ApplicationNotes on Assigned Demonstrations',
+    'View All ApplicationTagAssignments',
+    'View ApplicationTagAssignments on Assigned Demonstrations',
+    'View All DemonstrationTypeTagAssignments',
+    'View DemonstrationTypeTagAssignments on Assigned Demonstrations',
+    'View All DeliverableDemonstrationTypes',
+    'View DeliverableDemonstrationTypes on Assigned Demonstrations',
+    'View All DemonstrationRoleAssignments',
+    'View DemonstrationRoleAssignments on Assigned Demonstrations',
+    'View All ApplicationTagSuggestions',
+    'View ApplicationTagSuggestions on Assigned Demonstrations',
+    'View All People',
+    'View People on Assigned Demonstrations',
+    'View My Person',
+    'View All SystemRoleAssignments',
+    'View My SystemRoleAssignments'
+  )
+;
+
+DELETE FROM 
+  demos_app.permission
+WHERE
+  id IN (
+    'View All ApplicationPhases',
+    'View ApplicationPhases on Assigned Demonstrations',
+    'View All ApplicationDates',
+    'View ApplicationDates on Assigned Demonstrations',
+    'View All ApplicationNotes',
+    'View ApplicationNotes on Assigned Demonstrations',
+    'View All ApplicationTagAssignments',
+    'View ApplicationTagAssignments on Assigned Demonstrations',
+    'View All DemonstrationTypeTagAssignments',
+    'View DemonstrationTypeTagAssignments on Assigned Demonstrations',
+    'View All DeliverableDemonstrationTypes',
+    'View DeliverableDemonstrationTypes on Assigned Demonstrations',
+    'View All DemonstrationRoleAssignments',
+    'View DemonstrationRoleAssignments on Assigned Demonstrations',
+    'View All ApplicationTagSuggestions',
+    'View ApplicationTagSuggestions on Assigned Demonstrations',
+    'View All People',
+    'View People on Assigned Demonstrations',
+    'View My Person',
+    'View All SystemRoleAssignments',
+    'View My SystemRoleAssignments'
+  )
+;
