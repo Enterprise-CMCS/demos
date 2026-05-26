b513d62448750ffa2c6a97331aa75122fe0e1f6e
Connor J. Parke <connor.parke@icf.com>
2026-05-26 16:46:05 +0000

    DEMOS-184: updated alt text

--- client/src/components/brand/Logo.test.tsx
+++ client/src/components/brand/Logo.test.tsx
@@ -5,7 +5,9 @@ import { Logo } from "./Logo";
 describe("Logo", () => {
   it("renders the logo image with correct src and alt attributes", () => {
     render(<Logo />);
-    const logoImage = screen.getByAltText("Logo") as HTMLImageElement;
+    const logoImage = screen.getByAltText(
+      "DEMOS Logo with text: DEMOS, Demonstration Evaluation Management & Oversight System"
+    ) as HTMLImageElement;
     expect(logoImage).toBeInTheDocument();
     expect(logoImage.src).toContain("/img/logo.svg");
   });
--- client/src/components/brand/Logo.tsx
+++ client/src/components/brand/Logo.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 
 const LOGO_SRC = "/img/logo.svg";
+const LOGO_ALT =
+  "DEMOS Logo with text: DEMOS, Demonstration Evaluation Management & Oversight System";
 
 export const Logo: React.FC = () => {
   return (
     <a href="/" className="h-[40px]">
-      <img height="40px" src={LOGO_SRC} alt="Logo" />
+      <img height="40px" src={LOGO_SRC} alt={LOGO_ALT} />
     </a>
   );
 };
