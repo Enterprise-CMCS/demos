ec5c4fe5c6ad1858ebc0917670f5771d6f93b428
dustbuster <dustin.h@globalalliantinc.com>
2026-05-28 21:20:17 +0000

    DEMOS-2171: added constraint for deliverable trim

---
+++ server/src/model/migrations/20260528211105_check_deliverable_name_whitespace/migration.sql
@@ -0,0 +1,20 @@
+SET search_path TO demos_app;
+
+BEGIND;
+
+UPDATE
+    demos_app.deliverable
+SET
+    name = trim(name)
+WHERE
+    name <> trim(name);
+
+ALTER TABLE
+    demos_app.deliverable
+ADD CONSTRAINT
+    check_deliverable_name_has_no_leading_trailing_whitespace
+CHECK (
+    name = trim(name)
+);
+
+COMMIT;
