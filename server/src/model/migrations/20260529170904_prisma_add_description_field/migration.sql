b27ed549a72bb8c2a94750e217a44e429d20d6ae
Tom Rehman <tom.rehman@icf.com>
2026-05-29 13:57:54 -0400

    note unnecessary now that I updated the migration

--- server/src/model/migrations/20260529170904_prisma_add_description_field/migration.sql
+++ server/src/model/migrations/20260529170904_prisma_add_description_field/migration.sql
@@ -1,11 +1,3 @@
-/*
-  Warnings:
-
-  - Added the required column `description` to the `reference` table without a default value. This is not possible if the table is not empty.
-  - Added the required column `description` to the `reference_history` table without a default value. This is not possible if the table is not empty.
-
-*/
-
 SET search_path TO demos_app;
 
 -- AlterTable
