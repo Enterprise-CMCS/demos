0ffba9cd21cfa008efee19318cd123548559527d
Jeff <Jeffrey.M@globalalliantinc.com>
2026-06-02 14:24:45 +0000

    DEMOS-2109 be more explicit about nulls

--- server/src/model/migrations/20260602115947_check_signature_level/migration.sql
+++ server/src/model/migrations/20260602115947_check_signature_level/migration.sql
@@ -1,11 +1,20 @@
 ALTER TABLE demos_app.demonstration
 ADD CONSTRAINT demonstration_signature_level_check
-CHECK (signature_level_id = 'OA');
+CHECK (
+  signature_level_id IS NOT NULL
+  AND signature_level_id = 'OA'
+);
 
 ALTER TABLE demos_app.amendment
 ADD CONSTRAINT amendment_signature_level_check
-CHECK (signature_level_id IN ('OA', 'OCD'));
+CHECK (
+  signature_level_id IS NULL
+  OR signature_level_id IN ('OA', 'OCD')
+);
 
 ALTER TABLE demos_app.extension
 ADD CONSTRAINT extension_signature_level_check
-CHECK (signature_level_id IN ('OA', 'OCD'));
+CHECK (
+  signature_level_id IS NULL
+  OR signature_level_id IN ('OA', 'OCD')
+);
