60e5a27d436c458ef43951523b239cbdfa127166
Tresdon Jones <tresdon.jones@icf.com>
2026-06-05 12:01:55 -0400

    DEMOS-2210: 508 Compliance - `<hr>` elements get `aria-hidden` (#1382)
    rule elements get aria-hidden

--- client/src/components/application/extension/ExtensionWorkflow.tsx
+++ client/src/components/application/extension/ExtensionWorkflow.tsx
@@ -49,22 +49,9 @@ export const GET_EXTENSION_WORKFLOW_QUERY = gql`
   ${WORKFLOW_DOCUMENT_FIELDS}
 `;
 
-export type ApplicationWorkflowExtension =
-  WorkflowApplication &
-  Pick<
-    Extension,
-    "name" |
-    "description" |
-    "effectiveDate" |
-    "signatureLevel" |
-    "status"
-  > & {
-    demonstration: Pick<
-        Demonstration,
-        | "id"
-        | "status"
-        | "medicaidId"
-      > & {
+export type ApplicationWorkflowExtension = WorkflowApplication &
+  Pick<Extension, "name" | "description" | "effectiveDate" | "signatureLevel" | "status"> & {
+    demonstration: Pick<Demonstration, "id" | "status" | "medicaidId"> & {
       demonstrationTypes: Pick<
         DemonstrationTypeAssignment,
         | "demonstrationTypeName"
@@ -94,7 +81,7 @@ export const ExtensionWorkflow = ({ extensionId }: { extensionId: string }) => {
           <h3 className="text-brand text-2xl font-bold">APPLICATION</h3>
           <ApplicationStatusBadge applicationStatus={data.extension.status} />
         </div>
-        <hr className="text-border-rules" />
+        <hr className="text-border-rules" aria-hidden="true" />
         <PhaseSelector application={data.extension} workflowApplicationType="extension" />
       </div>
     );
