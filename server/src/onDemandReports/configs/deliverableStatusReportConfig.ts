2b1a794de65d49de099ed150924d7b150c0ee155
Tom Rehman <tom.rehman@icf.com>
2026-06-11 10:05:41 -0400

    DEMOS-1819-implement-deliverable-status-report (#1415)
    * Adds sqlfluff which I used to validate / format my sql. Updates the on demand resolver slightly to have more useful Zod errors. Updates deliverable status report with new query and updates the schema accordingly.
    
    * Step one of two to change case.
    
    * Step two of two to change case.
    
    * Smells, add small test cases for custom schema types

--- server/src/onDemandReports/configs/deliverableStatusReportConfig.ts
+++ server/src/onDemandReports/configs/deliverableStatusReportConfig.ts
@@ -1,30 +1,114 @@
 import { z } from "zod";
-import { APPLICATION_STATUS, STATES_AND_TERRITORIES } from "../../constants";
+import {
+  DELIVERABLE_ACTION_TYPES,
+  DELIVERABLE_EXTENSION_REASON_CODES,
+  DELIVERABLE_STATUSES,
+  DELIVERABLE_TYPES,
+  STATES_AND_TERRITORIES,
+} from "../../constants";
 import {
   OnDemandReportColumnHeader,
   OnDemandReportColumnSchema,
   OnDemandReportConfiguration,
 } from "./onDemandReportConfigTypes";
 import { deliverableStatusReportQuery } from "./deliverableStatusReportQuery";
+import { usDateString, usDateStringOrDash } from "./onDemandReportCustomSchemaTypes";
 
-type DeliverableStatusReportColumn = "id" | "name" | "description" | "state" | "status";
+type DeliverableStatusReportColumn =
+  | "state"
+  | "demonstration_title"
+  | "demonstration_number"
+  | "effective_date"
+  | "expiration_date"
+  | "primary_project_officer"
+  | "ddme_analyst"
+  | "deliverable_type"
+  | "deliverable_name"
+  | "demonstration_types"
+  | "cms_owner"
+  | "due_date"
+  | "submission_date"
+  | "extension_requested"
+  | "extension_date_requested"
+  | "reason_for_extension"
+  | "extension_request_comments"
+  | "total_extensions_requested"
+  | "resubmission_date"
+  | "resubmission_request_comments"
+  | "total_resubmissions_requested"
+  | "deliverable_status"
+  | "last_deliverable_action"
+  | "last_deliverable_action_date"
+  | "updated_post_acceptance"
+  | "deliverable_reviewer"
+  | "deliverable_review_date"
+  | "budget_neutrality_variance"
+  | "actuals";
 
 const deliverableStatusReportSchema = z
   .object({
-    id: z.string(),
-    name: z.string(),
-    description: z.string().nullable(),
     state: z.enum(STATES_AND_TERRITORIES.map((state) => state.id)),
-    status: z.enum(APPLICATION_STATUS),
+    demonstration_title: z.string(),
+    demonstration_number: z.string(),
+    effective_date: usDateString,
+    expiration_date: usDateString,
+    primary_project_officer: z.string(),
+    ddme_analyst: z.string(),
+    deliverable_type: z.enum(DELIVERABLE_TYPES),
+    deliverable_name: z.string(),
+    demonstration_types: z.string(),
+    cms_owner: z.string(),
+    due_date: usDateString,
+    submission_date: usDateStringOrDash,
+    extension_requested: z.enum(["Yes", "No"]),
+    extension_date_requested: usDateStringOrDash,
+    reason_for_extension: z.enum([...DELIVERABLE_EXTENSION_REASON_CODES, "-"]),
+    extension_request_comments: z.string(),
+    total_extensions_requested: z.int(),
+    resubmission_date: usDateStringOrDash,
+    resubmission_request_comments: z.string(),
+    total_resubmissions_requested: z.int(),
+    deliverable_status: z.enum(DELIVERABLE_STATUSES),
+    last_deliverable_action: z.enum(DELIVERABLE_ACTION_TYPES),
+    last_deliverable_action_date: usDateString,
+    updated_post_acceptance: z.enum(["Yes", "No"]),
+    deliverable_reviewer: z.string(),
+    deliverable_review_date: usDateStringOrDash,
+    budget_neutrality_variance: z.string(),
+    actuals: z.enum(["Actual", "Actual + Projected", "-"]),
   } satisfies OnDemandReportColumnSchema<DeliverableStatusReportColumn>)
   .strict();
 
 const deliverableStatusReportColumnHeaders = {
-  id: "ID",
-  name: "Demonstration Name",
-  description: "Demonstration Description",
-  state: "State Code",
-  status: "Demonstration Status",
+  state: "State/Territory",
+  demonstration_title: "Demonstration Title",
+  demonstration_number: "Demonstration Number",
+  effective_date: "Effective Date",
+  expiration_date: "Expiration Date",
+  primary_project_officer: "Primary Project Officer",
+  ddme_analyst: "DDME Analyst",
+  deliverable_type: "Deliverable Type",
+  deliverable_name: "Deliverable Name",
+  demonstration_types: "Demonstration Type",
+  cms_owner: "CMS Owner",
+  due_date: "Due Date",
+  submission_date: "Submission Date",
+  extension_requested: "Extension Requested",
+  extension_date_requested: "Extension Date Requested",
+  reason_for_extension: "Reason for Extension",
+  extension_request_comments: "Extension Request Comments",
+  total_extensions_requested: "Total Extensions Requested",
+  resubmission_date: "Resubmission Date",
+  resubmission_request_comments: "Resubmission Request Comments",
+  total_resubmissions_requested: "Total Resubmissions Requested",
+  deliverable_status: "Deliverable Status",
+  last_deliverable_action: "Last Deliverable Action",
+  last_deliverable_action_date: "Last Deliverable Action Date",
+  updated_post_acceptance: "Updated Post Acceptance",
+  deliverable_reviewer: "Deliverable Reviewer",
+  deliverable_review_date: "Deliverable Review Date",
+  budget_neutrality_variance: "Budget Neutrality Variance",
+  actuals: "Actuals",
 } satisfies OnDemandReportColumnHeader<DeliverableStatusReportColumn>;
 
 export const deliverableStatusReportConfiguration = {
--- server/src/onDemandReports/configs/deliverableStatusReportQuery.ts
+++ server/src/onDemandReports/configs/deliverableStatusReportQuery.ts
@@ -1,13 +1,258 @@
 export const deliverableStatusReportQuery = `
+WITH primary_roles AS (
+    SELECT
+        p.first_name || ' ' || p.last_name AS full_name,
+        pdra.demonstration_id,
+        pdra.role_id
+    FROM
+        demos_app.primary_demonstration_role_assignment AS pdra
+    INNER JOIN
+        demos_app.person AS p
+        ON
+            pdra.person_id = p.id
+),
+
+flat_demonstration_types AS (
+    SELECT
+        deliverable_id,
+        string_agg(demonstration_type_tag_name_id, ', ' ORDER BY demonstration_type_tag_name_id) AS demonstration_types
+    FROM
+        demos_app.deliverable_demonstration_type
+    GROUP BY
+        deliverable_id
+),
+
+
+action_statistics AS (
+    SELECT DISTINCT
+        deliverable_id,
+        max(CASE WHEN action_type_id = 'Submitted Deliverable' THEN action_timestamp END)
+            OVER (PARTITION BY deliverable_id)
+            AS most_recent_submission_timestamp,
+        max(CASE WHEN action_type_id = 'Requested Resubmission' THEN action_timestamp END)
+            OVER (PARTITION BY deliverable_id)
+            AS most_recent_resubmission_request_timestamp,
+        max(
+            CASE
+                WHEN
+                    action_type_id IN (
+                        'Accepted Deliverable',
+                        'Approved Deliverable',
+                        'Received and Filed Deliverable'
+                    )
+                    THEN action_timestamp
+            END
+        ) OVER (PARTITION BY deliverable_id) AS most_recent_finalization_timestamp,
+        sum(CASE WHEN action_type_id = 'Requested Extension' THEN 1 ELSE 0 END)
+            OVER (PARTITION BY deliverable_id)::INT
+            AS n_requested_extensions,
+        sum(CASE WHEN action_type_id = 'Requested Resubmission' THEN 1 ELSE 0 END)
+            OVER (PARTITION BY deliverable_id)::INT
+            AS n_requested_resubmissions,
+        max(action_timestamp) OVER (PARTITION BY deliverable_id) AS most_recent_action_timestamp,
+        first_value(action_type_id)
+            OVER (PARTITION BY deliverable_id ORDER BY action_timestamp DESC)
+            AS most_recent_action_type
+    FROM
+        demos_app.deliverable_action
+),
+
+-- Only one extension can be Requested at a time by DB rules
+-- Means we don't need to mess with first_value(), etc
+current_extensions AS (
+    SELECT
+        'Yes' AS extension_requested,
+        de.id,
+        de.deliverable_id,
+        de.original_date_requested,
+        de.reason_code_id,
+        da.note
+    FROM
+        demos_app.deliverable_extension AS de
+    INNER JOIN
+        demos_app.deliverable_action AS da
+        ON
+            de.id = da.active_extension_id
+            AND da.action_type_id = 'Requested Extension'
+            AND de.status_id = 'Requested'
+),
+
+most_recent_resub_reqs AS (
+    SELECT DISTINCT
+        deliverable_id,
+        first_value(id) OVER (PARTITION BY deliverable_id ORDER BY action_timestamp DESC) AS id,
+        first_value(note) OVER (PARTITION BY deliverable_id ORDER BY action_timestamp DESC) AS note,
+        first_value(new_due_date) OVER (PARTITION BY deliverable_id ORDER BY action_timestamp DESC) AS new_due_date
+    FROM
+        demos_app.deliverable_action
+    WHERE
+        action_type_id = 'Requested Resubmission'
+),
+
+most_recent_public_comment AS (
+    SELECT
+        deliverable_id,
+        'Yes' AS updated_post_acceptance,
+        max(created_at) AS created_at
+    FROM
+        demos_app.public_comment
+    GROUP BY
+        deliverable_id
+),
+
+most_recent_review_start AS (
+    SELECT DISTINCT
+        da.deliverable_id,
+        first_value(p.first_name || ' ' || p.last_name)
+            OVER (PARTITION BY da.deliverable_id ORDER BY da.action_timestamp DESC)
+            AS full_name,
+        max(da.action_timestamp) OVER (PARTITION BY da.deliverable_id) AS action_timestamp
+    FROM
+        demos_app.deliverable_action AS da
+    INNER JOIN
+        demos_app.person AS p
+        ON
+            da.user_id = p.id AND da.action_type_id = 'Started Review'
+),
+
+most_recent_valid_bn_data AS (
+    SELECT DISTINCT
+        doc.deliverable_id,
+        first_value(bn.net_variance_total)
+            OVER (PARTITION BY da.deliverable_id ORDER BY da.action_timestamp DESC)
+            AS net_variance_total,
+        first_value(bn.actuals) OVER (PARTITION BY da.deliverable_id ORDER BY da.action_timestamp DESC) AS actuals
+    FROM
+        demos_app.budget_neutrality_workbook AS bn
+
+    -- Get to the deliverable from the document
+    INNER JOIN
+        demos_app.document AS doc
+        ON
+            bn.id = doc.id
+
+    -- Inner join here filters those not submitted
+    -- Also filters non-successes
+    INNER JOIN
+        demos_app.deliverable_action AS da
+        ON
+            doc.deliverable_submission_action_id = da.id
+            AND bn.validation_status_id = 'Succeeded'
+)
+
 SELECT
-    id,
-    name,
-    description,
-    state_id AS state,
-    status_id AS status
+    demo.state_id AS state,
+    demo.name AS demonstration_title,
+    demo.medicaid_id AS demonstration_number,
+    to_char(demo.effective_date AT TIME ZONE 'America/New_York', 'MM/DD/YYYY') AS effective_date,
+    to_char(demo.expiration_date AT TIME ZONE 'America/New_York', 'MM/DD/YYYY') AS expiration_date,
+    primary_project_officer.full_name AS primary_project_officer,
+    coalesce(primary_ddme_analyst.full_name, '-') AS ddme_analyst,
+    deliv.deliverable_type_id AS deliverable_type,
+    deliv.name AS deliverable_name,
+    demo_types.demonstration_types,
+    cms_owner.first_name || ' ' || cms_owner.last_name AS cms_owner,
+    to_char(deliv.due_date AT TIME ZONE 'America/New_York', 'MM/DD/YYYY') AS due_date,
+    coalesce(to_char(act_stats.most_recent_submission_timestamp AT TIME ZONE 'America/New_York', 'MM/DD/YYYY'), '-')
+        AS submission_date,
+    coalesce(current_extensions.extension_requested, 'No') AS extension_requested,
+    coalesce(to_char(current_extensions.original_date_requested AT TIME ZONE 'America/New_York', 'MM/DD/YYYY'), '-')
+        AS extension_date_requested,
+    coalesce(current_extensions.reason_code_id, '-') AS reason_for_extension,
+    coalesce(current_extensions.note, '-') AS extension_request_comments,
+    act_stats.n_requested_extensions AS total_extensions_requested,
+    coalesce(to_char(most_recent_resub_reqs.new_due_date AT TIME ZONE 'America/New_York', 'MM/DD/YYYY'), '-')
+        AS resubmission_date,
+    coalesce(most_recent_resub_reqs.note, '-') AS resubmission_request_comments,
+    act_stats.n_requested_resubmissions AS total_resubmissions_requested,
+    deliv.status_id AS deliverable_status,
+    act_stats.most_recent_action_type AS last_deliverable_action,
+    to_char(act_stats.most_recent_action_timestamp AT TIME ZONE 'America/New_York', 'MM/DD/YYYY')
+        AS last_deliverable_action_date,
+    coalesce(most_recent_public_comment.updated_post_acceptance, 'No') AS updated_post_acceptance,
+    coalesce(most_recent_review_start.full_name, '-') AS deliverable_reviewer,
+    coalesce(to_char(most_recent_review_start.action_timestamp AT TIME ZONE 'America/New_York', 'MM/DD/YYYY'), '-')
+        AS deliverable_review_date,
+    coalesce(most_recent_valid_bn_data.net_variance_total::TEXT, '-') AS budget_neutrality_variance,
+    coalesce(most_recent_valid_bn_data.actuals, '-') AS actuals
 FROM
-    demos_app.demonstration
-ORDER BY
-    created_at DESC
-LIMIT 5;
+    demos_app.deliverable AS deliv
+
+-- All deliverables have demonstrations
+INNER JOIN
+    demos_app.demonstration AS demo
+    ON
+        deliv.demonstration_id = demo.id
+
+-- Primary Project Officer is guaranteed to exist
+INNER JOIN
+    primary_roles AS primary_project_officer
+    ON
+        deliv.demonstration_id = primary_project_officer.demonstration_id
+        AND primary_project_officer.role_id = 'Project Officer'
+
+-- Primary DDME Analyst is not guaranteed to exist
+LEFT JOIN
+    primary_roles AS primary_ddme_analyst
+    ON
+        deliv.demonstration_id = primary_ddme_analyst.demonstration_id
+        AND primary_ddme_analyst.role_id = 'DDME Analyst'
+
+-- Approved demonstrations are required to have at least one demonstration type
+INNER JOIN
+    flat_demonstration_types AS demo_types
+    ON
+        deliv.id = demo_types.deliverable_id
+
+-- All deliverables have CMS owners
+INNER JOIN
+    demos_app.person AS cms_owner
+    ON
+        deliv.cms_owner_user_id = cms_owner.id
+
+-- All deliverables will have action statistics
+INNER JOIN
+    action_statistics AS act_stats
+    ON
+        deliv.id = act_stats.deliverable_id
+
+-- Not all deliverables have active extension requests
+LEFT JOIN
+    current_extensions
+    ON
+        deliv.id = current_extensions.deliverable_id
+
+-- Not all deliverables have current resubmission requests
+-- Define current request as one where most recent resub request after most recent submission
+LEFT JOIN
+    most_recent_resub_reqs
+    ON
+        deliv.id = most_recent_resub_reqs.deliverable_id
+        AND act_stats.most_recent_submission_timestamp
+        < act_stats.most_recent_resubmission_request_timestamp
+
+-- Not all deliverables have comments
+-- Filter joined rows based on deliverable status and dates
+LEFT JOIN
+    most_recent_public_comment
+    ON
+        deliv.id = most_recent_public_comment.deliverable_id
+        AND deliv.status_id IN ('Accepted', 'Approved', 'Received and Filed')
+        AND act_stats.most_recent_finalization_timestamp < most_recent_public_comment.created_at
+
+-- Not all deliverables have reviews started
+LEFT JOIN
+    most_recent_review_start
+    ON
+        deliv.id = most_recent_review_start.deliverable_id
+
+-- Not all deliverables have BN data
+LEFT JOIN
+    most_recent_valid_bn_data
+    ON
+        deliv.id = most_recent_valid_bn_data.deliverable_id
+
+-- Finally: don't show deleted deliverables
+WHERE
+    deliv.status_id != 'Deleted';
 `;
