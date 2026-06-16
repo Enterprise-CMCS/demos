5f3e1a1170ebf5ac975de34713b048de2c94820e
Tom Rehman <tom.rehman@icf.com>
2026-06-16 17:58:05 -0400

    Stupid delay in my file system bit me here

--- server/src/onDemandReports/configs/index.ts
+++ server/src/onDemandReports/configs/index.ts
@@ -5,12 +5,14 @@ import { basicTestReportConfiguration } from "./basicTestReportConfig";
 import { deliverableStatusReportConfiguration } from "./deliverableStatusReportConfig";
 import { applicationDetailsReportConfiguration } from "./applicationDetailsReportConfig";
 import { demonstrationOverviewReportConfiguration } from "./demonstrationOverviewReportConfig";
+import { demonstrationTypesReportConfiguration } from "./demonstrationTypesReportConfig";
 
 export const ON_DEMAND_REPORT_CONFIGURATIONS = {
   "Basic Test Report": basicTestReportConfiguration,
   "Deliverable Status Report": deliverableStatusReportConfiguration,
   "Application Details Report": applicationDetailsReportConfiguration,
   "Demonstration Overview Report": demonstrationOverviewReportConfiguration,
+  "Demonstration Types Report": demonstrationTypesReportConfiguration,
 } satisfies Record<OnDemandReportType, OnDemandReportConfiguration>;
 
 export function getOnDemandReportConfiguration<T extends OnDemandReportType>(
