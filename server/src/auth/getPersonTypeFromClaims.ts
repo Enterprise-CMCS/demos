758cd14dca05dea1933bf7839306ac7f2040e198
Tom Rehman <tom.rehman@icf.com>
2026-06-23 14:46:50 -0400

    Code smell

--- server/src/auth/getPersonTypeFromClaims.ts
+++ server/src/auth/getPersonTypeFromClaims.ts
@@ -3,8 +3,8 @@ import type { UserType } from "../types";
 import type { AuthorizationClaims } from "./auth.util";
 
 export function getPersonTypeFromClaims(claims: AuthorizationClaims): UserType {
-  const claimsRoles = claims.role.split(",").map((role) => role.trim());
-  const applicableRoles = USER_TYPES.filter((userType) => claimsRoles.includes(userType));
+  const claimsRoles = new Set(claims.role.split(",").map((role) => role.trim()));
+  const applicableRoles = USER_TYPES.filter((userType) => claimsRoles.has(userType));
   if (applicableRoles.length === 0) {
     throw new Error(`User with cognito subject ${claims.sub} does not have any applicable roles`);
   } else if (applicableRoles.length > 1) {
