6b3be2cdfa919e65fdc1ef0c2440f84c328c6cbf
Tom Rehman <tom.rehman@icf.com>
2026-06-23 16:38:41 -0400

    improved logging

--- server/src/auth/getPersonTypeFromClaims.ts
+++ server/src/auth/getPersonTypeFromClaims.ts
@@ -1,4 +1,5 @@
 import { USER_TYPES } from "../constants";
+import { log } from "../log";
 import type { UserType } from "../types";
 import type { AuthorizationClaims } from "./auth.util";
 
@@ -6,11 +7,13 @@ export function getPersonTypeFromClaims(claims: AuthorizationClaims): UserType {
   const claimsRoles = new Set(claims.role.split(",").map((role) => role.trim()));
   const applicableRoles = USER_TYPES.filter((userType) => claimsRoles.has(userType));
   if (applicableRoles.length === 0) {
-    throw new Error(`User with cognito subject ${claims.sub} does not have any applicable roles`);
+    const errorMessage = `User with cognito subject ${claims.sub} does not have any applicable roles`;
+    log.error(errorMessage);
+    throw new Error(errorMessage);
   } else if (applicableRoles.length > 1) {
-    throw new Error(
-      `Claims with cognito subject ${claims.sub} has multiple applicable roles: ${applicableRoles.join(", ")}`
-    );
+    const errorMessage = `Claims with cognito subject ${claims.sub} has multiple applicable roles: ${applicableRoles.join(", ")}`;
+    log.error(errorMessage);
+    throw new Error(errorMessage);
   }
 
   return applicableRoles[0];
