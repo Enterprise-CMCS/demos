4f5064f8320e6d00ee15adfc86a5da0d0a18bca0
James Becwar <james.becwar@icf.com>
2026-06-24 19:44:57 +0000

    Add example bn woorkbook for test.

--- shared_library/src/BN/validation.test.ts
+++ shared_library/src/BN/validation.test.ts
@@ -1,9 +1,21 @@
 import { describe, it, expect, vi } from "vitest";
 
 import type { ExcelData } from "./index.js";
+import * as BN from "./index.js";
 import { validateBNWorkbook, type ValidationError } from "./validation.js";
+import { validations as v1Validations } from "./rulesets/v1/index.js";
+import { extractorFunctions } from "./extractors/index.js";
 
 describe("validateBNWorkbook", () => {
+  it("should validate a real file", async() => {
+    const data = await BN.parseBNFileFromPath("test/fixtures/test-bn.xlsm");
+    const result = await validateBNWorkbook(data, v1Validations, extractorFunctions);    
+    expect(result.isValid).toBe(true);
+    expect(result.extractedValues?.get("netVariance")).toBe(46848737436.56)
+    expect(result.extractedValues?.get("actuals")).toBe("Actuals + Projected");
+    console.log(result);
+  });
+
   it("returns a valid result with empty errors when no validations are provided", async () => {
     const data: ExcelData = [];
 
