4b29caf32cdd85c862a04c3690aead86b1dde0d2
Tom Rehman <tom.rehman@icf.com>
2026-06-24 16:03:41 -0400

    Very basic start of the metadata stuff

--- server/src/onDemandReports/formatOnDemandReportInExcel.ts
+++ server/src/onDemandReports/formatOnDemandReportInExcel.ts
@@ -1,23 +1,43 @@
-import { OnDemandReportType } from "../types";
-import { getOnDemandReportConfiguration, OnDemandReportRow } from "./configs";
+import type { EasternTZDate } from "../dateUtilities";
+import type { OnDemandReportType } from "../types";
+import type { OnDemandReportRow } from "./configs";
+
+import { getOnDemandReportConfiguration } from "./configs";
 import { Workbook } from "@cj-tech-master/excelts";
+
+type ReportMetadata = {
+  requestId: string;
+  requestTimestamp: EasternTZDate;
+};
 
 export async function formatOnDemandReportInExcel<T extends OnDemandReportType>(
   reportType: T,
-  rows: OnDemandReportRow<T>[]
+  rows: OnDemandReportRow<T>[],
+  reportMetadata: ReportMetadata
 ): Promise<Buffer> {
   const workbook = new Workbook();
-  const worksheet = workbook.addWorksheet(reportType);
+  const primaryWorksheet = workbook.addWorksheet(reportType);
+  const metadataWorksheet = workbook.addWorksheet("Report Metadata");
 
   const { columnNames } = getOnDemandReportConfiguration(reportType).excelConfiguration;
-  worksheet.columns = Object.entries(columnNames).map(([key, header]) => ({ key, header }));
+  primaryWorksheet.columns = Object.entries(columnNames).map(([key, header]) => ({ key, header }));
 
   for (const row of rows) {
-    worksheet.addRow(row);
+    primaryWorksheet.addRow(row);
   }
 
-  worksheet.getRow(1).font = { bold: true };
-  worksheet.autoFitColumns();
+  primaryWorksheet.getRow(1).font = { bold: true };
+  primaryWorksheet.autoFitColumns();
+
+  metadataWorksheet.addRow(["Request Id", reportMetadata.requestId]);
+  metadataWorksheet.addRow([
+    "Request Timestamp",
+    reportMetadata.requestTimestamp.easternTZDate.toISOString(),
+  ]);
+  metadataWorksheet.getColumn(1).font = { bold: true };
+  metadataWorksheet.getColumn(2).alignment = { horizontal: "right" };
+  metadataWorksheet.getRow(2).getCell(2).numFmt = "@";
+  metadataWorksheet.autoFitColumns();
 
   const buffer = await workbook.xlsx.writeBuffer();
   return Buffer.from(buffer);
