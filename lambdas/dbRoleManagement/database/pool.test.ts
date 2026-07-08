44b7829bb986d99119be6d239b55a3c7e1ed0ac1
cms-jesse <107690848+cms-jesse@users.noreply.github.com>
2026-07-08 12:38:25 -0400

    DEMOS-2406: refactor dbRoleManagement to use vitest (#1539)
    * DEMOS-2406: refactor dbRoleManagement to use vitest
    
    * Remove jest config file

--- lambdas/dbRoleManagement/database/pool.test.ts
+++ lambdas/dbRoleManagement/database/pool.test.ts
@@ -1,38 +1,56 @@
-import * as pool from "./pool";
+import type * as poolModule from "./pool";
+import type * as envModule from "../util/env";
+import type * as secretsManagerModule from "@aws-sdk/client-secrets-manager";
+import type { Mock } from "vitest";
 
-import * as env from "../util/env";
-
-import {
-  SecretsManagerClient,
-  CreateSecretCommand,
-  RotateSecretCommand,
-  DeleteSecretCommand,
-} from "@aws-sdk/client-secrets-manager";
-
-jest.mock("@aws-sdk/client-secrets-manager");
+vi.mock("@aws-sdk/client-secrets-manager");
 
 describe("pool", () => {
-  afterEach(() => {
-    jest.restoreAllMocks();
-    jest.resetAllMocks();
+  let pool: typeof poolModule;
+  let env: typeof envModule;
+  let secretsManager: typeof secretsManagerModule;
+
+  beforeEach(async () => {
+    vi.resetModules();
+    // @ts-expect-error Vitest supports dynamic imports for resetting module state in tests.
+    pool = await import("./pool");
+    // @ts-expect-error 
+    env = await import("../util/env");
+    // @ts-expect-error 
+    secretsManager = await import("@aws-sdk/client-secrets-manager");
   });
+
+
   describe("getPool", () => {
     test("should return existing pool if it exists", async () => {
-      const spy = jest.spyOn(pool, "getDatabaseUrl").mockResolvedValue("databaseURL");
-      await pool.getPool();
-      expect(spy).toHaveBeenCalledTimes(1);
-      await pool.getPool();
-      expect(spy).toHaveBeenCalledTimes(1);
+      const mockArn = "unit-test-arn";
+      vi.spyOn(env, "getDBSecretArn").mockImplementation(() => mockArn);
+      const mockDBValues = {
+        dbname: "utdb",
+        engine: "postgres",
+        port: 5432,
+        dbInstanceIdentifier: "unit-test-rds",
+        host: "unit.test.rds.host",
+        password: "thisiafakepassword", // pragma: allowlist secret
+        username: "dbusername",
+      };
+      const sendMock = vi.fn().mockResolvedValue({ SecretString: JSON.stringify(mockDBValues) });
+      (secretsManager.SecretsManagerClient as unknown as Mock).mockImplementation(function() {return { send: sendMock }});
+
+      const firstPool = await pool.getPool();
+      const secondPool = await pool.getPool();
+
+      expect(firstPool).toBe(secondPool);
     });
   });
   describe("getDatabaseSecret", () => {
     test("should return secret if it exists", async () => {
       const mockArn = "unit-test-arn";
-      jest.spyOn(env, "getDBSecretArn").mockImplementation(() => mockArn);
+      vi.spyOn(env, "getDBSecretArn").mockImplementation(() => mockArn);
       const mockSecret = { mockValue: "testing" };
-      const sendMock = jest.fn().mockResolvedValue({ SecretString: JSON.stringify(mockSecret) });
-      (SecretsManagerClient as jest.Mock).mockImplementation(() => ({ send: sendMock }));
-      const getCmd = jest.spyOn(require("@aws-sdk/client-secrets-manager"), "GetSecretValueCommand");
+      const sendMock = vi.fn().mockResolvedValue({ SecretString: JSON.stringify(mockSecret) });
+      (secretsManager.SecretsManagerClient as unknown as Mock).mockImplementation(function() {return { send: sendMock }});
+      const getCmd = vi.spyOn(secretsManager, "GetSecretValueCommand");
 
       const resp = await pool.getDatabaseSecret();
       expect(getCmd).toHaveBeenCalledWith({ SecretId: mockArn });
@@ -45,6 +63,8 @@ describe("pool", () => {
   });
   describe("getDatabaseUrl", () => {
     test("should return properly formatted connection string", async () => {
+      const mockArn = "unit-test-arn";
+      vi.spyOn(env, "getDBSecretArn").mockImplementation(() => mockArn);
       const mockDBValues = {
         dbname: "utdb",
         engine: "postgres",
@@ -54,9 +74,13 @@ describe("pool", () => {
         password: "thisiafakepassword", // pragma: allowlist secret
         username: "dbusername",
       };
-      jest.spyOn(pool, "getDatabaseSecret").mockResolvedValue(mockDBValues);
+       const sendMock = vi.fn().mockResolvedValue({ SecretString: JSON.stringify(mockDBValues) });
+      (secretsManager.SecretsManagerClient as unknown as Mock).mockImplementation(function() {return { send: sendMock }});
+      const getCmd = vi.spyOn(secretsManager, "GetSecretValueCommand");
 
       const url = await pool.getDatabaseUrl();
+      // expect(sendMock).toHaveBeenCalled()
+
       expect(url).toEqual(
         `postgresql://${mockDBValues.username}:${mockDBValues.password}@${mockDBValues.host}:${mockDBValues.port}/${mockDBValues.dbname}?schema=demos_app`
       );
