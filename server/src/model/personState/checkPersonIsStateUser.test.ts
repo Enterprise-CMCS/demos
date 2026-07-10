e46ca56654bc8031edda778b42d762496282fcbf
Connor J. Parke <connor.parke@icf.com>
2026-07-10 20:21:45 +0000

    DEMOS-2021: updated test with correct mock

--- server/src/model/personState/checkPersonIsStateUser.test.ts
+++ server/src/model/personState/checkPersonIsStateUser.test.ts
@@ -1,10 +1,11 @@
 import { describe, it, expect, vi, beforeEach } from "vitest";
 import { checkPersonIsStateUser } from "./checkPersonIsStateUser";
-import { selectPersonOrThrow } from "../person/queries";
+import { selectPerson } from "../person/queries";
 import type { Person as PrismaPerson } from "@prisma/client";
+import { PersonType } from "../../types";
 
 vi.mock("../person/queries", () => ({
-  selectPersonOrThrow: vi.fn(),
+  selectPerson: vi.fn(),
 }));
 
 describe("checkPersonIsStateUser", () => {
@@ -16,29 +17,26 @@ describe("checkPersonIsStateUser", () => {
   });
 
   it("should return undefined if the person is a state user", async () => {
-    vi.mocked(selectPersonOrThrow).mockResolvedValue({
+    vi.mocked(selectPerson).mockResolvedValue({
       id: testPersonId,
       personTypeId: "demos-state-user",
     } as PrismaPerson);
 
     await expect(checkPersonIsStateUser(testPersonId, mockTransaction)).resolves.toBeUndefined();
-    expect(selectPersonOrThrow).toHaveBeenCalledExactlyOnceWith(
-      { id: testPersonId },
+    expect(selectPerson).toHaveBeenCalledExactlyOnceWith(
+      { id: testPersonId, personTypeId: "demos-state-user" as PersonType },
       mockTransaction
     );
   });
 
   it("should return an error message if the person is not a state user", async () => {
-    vi.mocked(selectPersonOrThrow).mockResolvedValue({
-      id: testPersonId,
-      personTypeId: "demos-cms-user",
-    } as PrismaPerson);
+    vi.mocked(selectPerson).mockResolvedValue(null);
 
     await expect(checkPersonIsStateUser(testPersonId, mockTransaction)).resolves.toBe(
       `Person ${testPersonId} is not a state user.`
     );
-    expect(selectPersonOrThrow).toHaveBeenCalledExactlyOnceWith(
-      { id: testPersonId },
+    expect(selectPerson).toHaveBeenCalledExactlyOnceWith(
+      { id: testPersonId, personTypeId: "demos-state-user" as PersonType },
       mockTransaction
     );
   });
