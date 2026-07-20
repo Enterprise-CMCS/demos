02db04e711bfc53e5f2f2233f27ecd45447069bd
Tom Rehman <tom.rehman@icf.com>
2026-07-20 14:28:28 -0400

    Removed these previously warning tests, and instead just test directly on the source data

--- data/migration/stage_pmda_for_migration/models/errors/errors_demos_with_missing_invalid_state_code.sql
+++
@@ -1,9 +0,0 @@
-SELECT *
-FROM {{ source('legacy_pmda_raw', 'mdcd_demo') }} AS d
-WHERE
-    geo_ansi_state_cd IS NULL
-    OR NOT EXISTS (
-        SELECT 1
-        FROM {{ source('demos_app', 'state') }} AS s
-        WHERE s.id = d.geo_ansi_state_cd
-    )
--- data/migration/stage_pmda_for_migration/tests/assert_no_pmda_demos_filtered_out_by_missing_state.sql
+++
@@ -1,7 +0,0 @@
--- Warn in cases where a PMDA demo is filtered out by missing a valid state
-
-{{ config(severity='warn') }}
-
-SELECT *
-FROM
-    {{ ref('errors_demos_with_missing_invalid_state_code') }}
