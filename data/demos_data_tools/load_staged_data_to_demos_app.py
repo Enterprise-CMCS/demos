0c61b3d2a7089f7bee12fb62f390e5b100bc9566
Tom Rehman <tom.rehman@icf.com>
2026-07-23 15:43:20 -0400

    Added basic action to show migration

--- data/demos_data_tools/load_staged_data_to_demos_app.py
+++ data/demos_data_tools/load_staged_data_to_demos_app.py
@@ -243,6 +243,27 @@ MIGRATION_CONFIGURATION: MigrationConfiguration = (
         ],
     ),
     TableInsertActionConfiguration(
+        "final_demos_app_deliverable_action",
+        "deliverable_action",
+        [
+            "id",
+            "action_timestamp",
+            "deliverable_id",
+            "action_type_id",
+            "old_status_id",
+            "new_status_id",
+            "note",
+            "active_extension_id",
+            "due_date_change_allowed",
+            "should_have_note",
+            "should_have_user_id",
+            "extension_id_optional",
+            "old_due_date",
+            "new_due_date",
+            "user_id",
+        ],
+    ),
+    TableInsertActionConfiguration(
         "final_demos_app_deliverable_demonstration_type",
         "deliverable_demonstration_type",
         [
