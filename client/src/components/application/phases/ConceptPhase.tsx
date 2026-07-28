7ac55655cfd0e1daa64d19870fad7f15c066d3d7
Tresdon Jones <tresdon.jones@icf.com>
2026-07-28 10:41:12 -0400

    (2/3) DEMOS-2386: Persist dates in first half of Application Workflow (#1624)
    * `useSessionStorage` on phases 1-3
    
    * Fix linting issue

--- client/src/components/application/phases/ConceptPhase.tsx
+++ client/src/components/application/phases/ConceptPhase.tsx
@@ -1,15 +1,12 @@
 import React, { useEffect, useState } from "react";
+import { useSessionStorage } from "hooks";
 import { compareDesc } from "date-fns";
 
 import { tw } from "tags/tw";
 import { Button, SecondaryButton } from "components/button";
 import { ChevronRightIcon, ExportIcon } from "components/icons";
 
-import {
-  WorkflowApplication,
-  ApplicationWorkflowDocument,
-  WorkflowApplicationType,
-} from "components/application";
+import { WorkflowApplication, ApplicationWorkflowDocument } from "components/application";
 import { formatDateForServer } from "util/formatDate";
 import { DocumentList } from "./sections";
 import { useDialog } from "components/dialog/DialogContext";
@@ -62,7 +59,6 @@ export const CONCEPT_PHASE_STEP_TWO_DESCRIPTION = {
 
 export const getConceptPhaseComponentFromApplication = (
   application: WorkflowApplication,
-  workflowApplicationType: WorkflowApplicationType,
   setSelectedPhase: (phase: PhaseName) => void
 ) => {
   const conceptPhaseDocuments = application.documents.filter(
@@ -135,7 +131,9 @@ export const ConceptPhase = ({
   const { skipConceptPhase } = useSkipConceptPhase();
 
   // User can override the calculated date via the datepicker
-  const [userSubmittedDateOverride, setUserSubmittedDateOverride] = useState<string>("");
+  const [userSubmittedDateOverride, setUserSubmittedDateOverride] = useSessionStorage(
+    `concept-phase-submitted-date-${applicationId}`
+  );
   const [isFinishEnabled, setIsFinishEnabled] = useState<boolean>(false);
   const [isSkipEnabled, setIsSkipEnabled] = useState<boolean>(true);
 
