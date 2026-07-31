9c3eaabaf58ba39a51f355983215053148afe710
Connor J. Parke <connor.parke@icf.com>
2026-07-31 14:32:43 +0000

    DEMOS-2446: added migrate flags for apps and documents

--- data/migration/stage_pmda_for_migration/models/cleaned/final/final_demos_app_application_date.sql
+++ data/migration/stage_pmda_for_migration/models/cleaned/final/final_demos_app_application_date.sql
@@ -1,1 +1,4 @@
-SELECT * FROM {{ ref('cleaned_demos_app_application_date_in_prog_demos') }}
+SELECT
+    *,
+    TRUE AS is_migrated_from_pmda
+FROM {{ ref('cleaned_demos_app_application_date_in_prog_demos') }}
