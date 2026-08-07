677fb231765cae5c3fea3300fdd9ed636859c2ef
Connor J. Parke <connor.parke@icf.com>
2026-08-07 20:28:15 +0000

    DEMOS-2532: small adjustments

--- data/migration/stage_pmda_for_migration/models/cleaned/final/final_demos_app_tag.sql
+++ data/migration/stage_pmda_for_migration/models/cleaned/final/final_demos_app_tag.sql
@@ -1,4 +1,8 @@
 SELECT pmda_tags.* FROM {{ ref('cleaned_demos_app_tag') }} AS pmda_tags
-WHERE pmda_tags.tag_name_id NOT IN (
-    SELECT demos_tags.tag_name_id FROM {{ source('demos_app', 'tag') }} AS demos_tags
-)
+WHERE
+    NOT EXISTS (
+        SELECT 1 FROM {{ source('demos_app', 'tag') }} AS demos_tags
+        WHERE
+            pmda_tags.tag_name_id = demos_tags.tag_name_id
+            AND pmda_tags.tag_type_id = demos_tags.tag_type_id
+    )
