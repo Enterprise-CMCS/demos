bc7eaaf666a3e3118a83a0f9f3847f06c2d222b4
Connor J. Parke <connor.parke@icf.com>
2026-08-13 14:33:39 +0000

    DEMOS-2483: added restriction to primary roles, initialized permissions on new role

---
+++ server/src/model/migrations/20260813131136_prisma_add_primary_demonstration_role_assignment_person_type_limit/migration.sql
@@ -0,0 +1,52 @@
+
+-- DropForeignKey
+ALTER TABLE "primary_demonstration_role_assignment" DROP CONSTRAINT "primary_demonstration_role_assignment_person_id_demonstrat_fkey";
+
+-- DropIndex
+DROP INDEX "primary_demonstration_role_assignment_person_id_demonstrati_key";
+
+-- AlterTable
+ALTER TABLE "demonstration_role_assignment" DROP CONSTRAINT "demonstration_role_assignment_pkey",
+ADD CONSTRAINT "demonstration_role_assignment_pkey" PRIMARY KEY ("person_id", "demonstration_id", "role_id", "person_type_id");
+
+-- AlterTable
+ALTER TABLE "primary_demonstration_role_assignment" ADD COLUMN     "person_type_id" TEXT;
+
+UPDATE "primary_demonstration_role_assignment" 
+SET "person_type_id" = "demonstration_role_assignment"."person_type_id" 
+FROM "demonstration_role_assignment" 
+WHERE 
+  "primary_demonstration_role_assignment"."person_id" = "demonstration_role_assignment"."person_id" 
+  AND "primary_demonstration_role_assignment"."demonstration_id" = "demonstration_role_assignment"."demonstration_id" 
+  AND "primary_demonstration_role_assignment"."role_id" = "demonstration_role_assignment"."role_id"
+;
+
+ALTER TABLE "primary_demonstration_role_assignment" ALTER COLUMN "person_type_id" SET NOT NULL;
+
+-- AlterTable
+ALTER TABLE "primary_demonstration_role_assignment_history" ADD COLUMN     "person_type_id" TEXT;
+
+-- CreateTable
+CREATE TABLE "primary_demonstration_role_assignment_person_type_limit" (
+    "id" TEXT NOT NULL,
+
+    CONSTRAINT "primary_demonstration_role_assignment_person_type_limit_pkey" PRIMARY KEY ("id")
+);
+
+INSERT INTO "primary_demonstration_role_assignment_person_type_limit" ("id") VALUES 
+  ('demos-admin'),
+  ('demos-cms-user'),
+  ('demos-state-user')
+;
+
+-- CreateIndex
+CREATE UNIQUE INDEX "primary_demonstration_role_assignment_person_id_demonstrati_key" ON "primary_demonstration_role_assignment"("person_id", "demonstration_id", "role_id", "person_type_id");
+
+-- AddForeignKey
+ALTER TABLE "primary_demonstration_role_assignment" ADD CONSTRAINT "primary_demonstration_role_assignment_person_id_demonstrat_fkey" FOREIGN KEY ("person_id", "demonstration_id", "role_id", "person_type_id") REFERENCES "demonstration_role_assignment"("person_id", "demonstration_id", "role_id", "person_type_id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "primary_demonstration_role_assignment" ADD CONSTRAINT "primary_demonstration_role_assignment_person_type_id_fkey" FOREIGN KEY ("person_type_id") REFERENCES "primary_demonstration_role_assignment_person_type_limit"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "primary_demonstration_role_assignment_person_type_limit" ADD CONSTRAINT "primary_demonstration_role_assignment_person_type_limit_id_fkey" FOREIGN KEY ("id") REFERENCES "person_type"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
---
+++ server/src/model/migrations/20260813133050_init_restricted_cms_user_role/migration.sql
@@ -0,0 +1,37 @@
+SET search_path TO demos_app;
+
+INSERT INTO person_type values ('demos-restricted-cms-user');
+
+INSERT INTO role values ('Restricted CMS User', 'System');
+
+INSERT INTO role_person_type VALUES
+    -- system roles
+    ('Restricted CMS User', 'demos-restricted-cms-user'),
+
+    -- demonstration role
+    ('Project Officer', 'demos-restricted-cms-user'),
+    ('DDME Analyst', 'demos-restricted-cms-user'),
+    ('Policy Technical Director', 'demos-restricted-cms-user'),
+    ('Monitoring & Evaluation Technical Director', 'demos-restricted-cms-user')
+;
+
+INSERT INTO role_permission VALUES
+	  -- field-based permissions
+    ('Restricted CMS User','System','Access CMS Field'),
+    ('Restricted CMS User','System','Access CMS Query'),
+
+    -- row-based permissions
+    ('Restricted CMS User','System','View All Demonstrations'),
+    ('Restricted CMS User','System','View Assigned Demonstrations'),
+    ('Restricted CMS User','System','View All Amendments'),
+    ('Restricted CMS User','System','View Amendments on Assigned Demonstrations'),
+    ('Restricted CMS User','System','View All Extensions'),
+    ('Restricted CMS User','System','View Extensions on Assigned Demonstrations'),
+    ('Restricted CMS User','System','View All Documents'),
+    ('Restricted CMS User','System','View All Deliverables'),
+    ('Restricted CMS User','System','View Deliverables on Assigned Demonstrations'),
+
+    ('Restricted CMS User','System','View All DemonstrationRoleAssignments'),
+    ('Restricted CMS User','System','View DemonstrationRoleAssignments on Assigned Demonstrations'),
+    ('Restricted CMS User','System','View Documents on Assigned Deliverables')
+;
