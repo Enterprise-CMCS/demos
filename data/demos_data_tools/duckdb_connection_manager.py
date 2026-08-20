cda0aaf4081cff164ff5375f5aef9550754112b7
Tom Rehman <tom.rehman@icf.com>
2026-08-20 14:06:50 -0400

    Removed the old PMDA exporter and began to update the DDB connection manager.

--- data/demos_data_tools/duckdb_connection_manager.py
+++ data/demos_data_tools/duckdb_connection_manager.py
@@ -1,4 +1,4 @@
-"""Shared utility for initializing a duckdb connection with both PMDA mysql and DEMOS postgres."""
+"""Shared utility for initializing a DuckDB connection."""
 
 import os
 from logging import getLogger
@@ -14,10 +14,10 @@ if TYPE_CHECKING:
 
 logger = config_logger(getLogger(__name__))
 
-PMDA_DDB_ATTACH_NAME = "ddb_pmda"
-DEMOS_DDB_ATTACH_NAME = "ddb_demos"
+DEMOS_LOCALSTACK_DDB_ATTACH_NAME = "ddb_demos_localstack"
+DEMOS_AWS_DDB_ATTACH_NAME = "ddb_demos_aws"
 
-type ConfigurationType = Literal["PMDA", "DEMOS"]
+type ConfigurationType = Literal["DEMOS LocalStack", "DEMOS AWS"]
 
 
 def _load_db_config_from_env(requested_config: ConfigurationType) -> dict:
@@ -33,47 +33,46 @@ def _load_db_config_from_env(requested_config: ConfigurationType) -> dict:
         KeyError: When loading from the environment fails due to a KeyError (missing env var).
         RuntimeError: When loading from the environment fails for non-KeyError reasons.
     """
-    logger.info(f"Attempting to load {requested_config} config from environment")
+    logger.info(f"Attempting to load {requested_config} DB config from environment")
 
     try:
-        if requested_config == "PMDA":
+        if requested_config == "DEMOS LocalStack":
             config = {
-                "host": os.environ["PMDA_MYSQL_HOST"],
-                "port": os.environ["PMDA_MYSQL_PORT"],
-                "user": os.environ["PMDA_MYSQL_USER"],
-                "pwd": os.environ["PMDA_MYSQL_PWD"],
-                "db": os.environ["PMDA_MYSQL_DB"],
+                "host": os.environ["DEMOS_LOCALSTACK_HOST"],
+                "port": os.environ["DEMOS_LOCALSTACK_PORT"],
+                "user": os.environ["DEMOS_LOCALSTACK_USER"],
+                "pwd": os.environ["DEMOS_LOCALSTACK_PWD"],
+                "db": os.environ["DEMOS_LOCALSTACK_DB"],
+                "sslmode": os.environ["DEMOS_LOCALSTACK_SSLMODE"],
             }
-        elif requested_config == "DEMOS":
+        elif requested_config == "DEMOS AWS":
             config = {
-                "host": os.environ["DEMOS_PGSQL_HOST"],
-                "port": os.environ["DEMOS_PGSQL_PORT"],
-                "user": os.environ["DEMOS_PGSQL_USER"],
-                "pwd": os.environ["DEMOS_PGSQL_PWD"],
-                "db": os.environ["DEMOS_PGSQL_DB"],
-                "sslmode": os.environ["DEMOS_PGSQL_SSLMODE"],
+                "host": os.environ["DEMOS_AWS_HOST"],
+                "port": os.environ["DEMOS_AWS_PORT"],
+                "user": os.environ["DEMOS_AWS_USER"],
+                "pwd": os.environ["DEMOS_AWS_PWD"],
+                "db": os.environ["DEMOS_AWS_DB"],
+                "sslmode": os.environ["DEMOS_AWS_SSLMODE"],
             }
         else:
             assert_never(requested_config)
     except KeyError:
-        err_msg = f"A KeyError occurred while loading the {requested_config} credentials from the environment."
+        err_msg = f"A KeyError occurred while loading the {requested_config} DB config from the environment."
         logger.error(err_msg)
         raise KeyError(err_msg) from None
     except Exception:
         err_msg = (
-            f"An unhandled exception occurred while loading the {requested_config} credentials from the environment."
+            f"An unhandled exception occurred while loading the {requested_config} DB config from the environment."
         )
         logger.error(err_msg)
         raise RuntimeError(err_msg) from None
 
-    logger.info(f"Loaded {requested_config} config from environment")
+    logger.info(f"Loaded {requested_config} DB config from environment")
     return config
 
 
 def create_duckdb_conn() -> "DuckConn":
     """Create a proper DuckDB connection with an in-memory DB and extensions mounted.
-
-    Note: this no longer mounts the databases as this is done in separate functions.
 
     Returns:
         DuckConn: An instantiated DuckDB connection.
@@ -81,52 +80,15 @@ def create_duckdb_conn() -> "DuckConn":
     logger.info("Creating DuckDB database")
     conn = duckdb.connect(":memory:", config={"memory_limit": "2GB", "threads": 2})
     conn.install_extension("postgres")
-    conn.install_extension("mysql")
     return conn
 
 
-def attach_pmda_to_conn(conn: "DuckConn") -> "DuckConn":
-    """Attach the PMDA database to a DuckDB connection.
-
-    Args:
-        conn (DuckConn): A DuckDB connection to which the PMDA database should be attached.
-
-    Returns:
-        DuckConn: The DuckDB connection with the attached PMDA database.
-
-    Raises:
-        RuntimeError: Generally raised if any connection issues occur; done to block leaking credentials in logs.
-    """
-    conn.load_extension("mysql")
-    ddb_pmda_config = _load_db_config_from_env("PMDA")
-    clean_pmda_pwd = ddb_pmda_config["pwd"].replace("'", "''")
-
-    try:
-        conn.execute(f"""
-            CREATE SECRET (
-                TYPE mysql,
-                HOST '{ddb_pmda_config["host"]}',
-                PORT {ddb_pmda_config["port"]},
-                DATABASE {ddb_pmda_config["db"]},
-                USER '{ddb_pmda_config["user"]}',
-                PASSWORD '{clean_pmda_pwd}'
-            );
-        """)
-        conn.execute(f"ATTACH '' AS {PMDA_DDB_ATTACH_NAME} (TYPE mysql);")
-    except Exception:
-        err_msg = "An error occurred while attempting to attach the PMDA database."
-        logger.error(err_msg)
-        raise RuntimeError(err_msg) from None
-
-    logger.info(f"Attached PMDA MySQL database AS {PMDA_DDB_ATTACH_NAME}")
-    return conn
-
-
-def attach_demos_to_conn(conn: "DuckConn") -> "DuckConn":
-    """Attach the DEMOS database to a DuckDB connection.
+def attach_demos_db_to_conn(conn: "DuckConn", config_type: ConfigurationType) -> "DuckConn":
+    """Attach a DEMOS database to a DuckDB connection.
 
     Args:
         conn (DuckConn): A DuckDB connection to which the DEMOS database should be attached.
+        config_type (ConfigurationType): Which DEMOS database to connect.
 
     Returns:
         DuckConn: The DuckDB connection with the attached DEMOS database.
@@ -135,8 +97,16 @@ def attach_demos_to_conn(conn: "DuckConn") -> "DuckConn":
         RuntimeError: Generally raised if any connection issues occur; done to block leaking credentials in logs.
     """
     conn.load_extension("postgres")
-    ddb_demos_config = _load_db_config_from_env("DEMOS")
+    ddb_demos_config = _load_db_config_from_env(config_type)
     clean_demos_pwd = ddb_demos_config["pwd"].replace("'", "''")
+
+    match config_type:
+        case "DEMOS LocalStack":
+            demos_ddb_attach_name = DEMOS_LOCALSTACK_DDB_ATTACH_NAME
+        case "DEMOS AWS":
+            demos_ddb_attach_name = DEMOS_AWS_DDB_ATTACH_NAME
+        case _:
+            assert_never(config_type)
 
     try:
         conn.execute(f"""
@@ -149,12 +119,12 @@ def attach_demos_to_conn(conn: "DuckConn") -> "DuckConn":
                 PASSWORD '{clean_demos_pwd}'
             );
         """)
-        conn.execute(f"ATTACH 'sslmode={ddb_demos_config['sslmode']}' AS {DEMOS_DDB_ATTACH_NAME} (TYPE postgres);")
+        conn.execute(f"ATTACH 'sslmode={ddb_demos_config['sslmode']}' AS {demos_ddb_attach_name} (TYPE postgres);")
         conn.execute("SET pg_null_byte_replacement=''")  # This is necessary to handle nulls from MySQL
     except Exception:
         err_msg = "An error occurred while attempting to attach the DEMOS database."
         logger.error(err_msg)
         raise RuntimeError(err_msg) from None
 
-    logger.info(f"Attached DEMOS PostgreSQL database AS {DEMOS_DDB_ATTACH_NAME}")
+    logger.info(f"Attached DEMOS database AS {demos_ddb_attach_name}")
     return conn
