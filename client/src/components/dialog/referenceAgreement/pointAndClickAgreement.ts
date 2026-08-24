5568701a786c4e76718cf66b67256a9a19c91648
dustbuster <dustin.h@globalalliantinc.com>
2026-08-24 17:49:02 +0000

    DEMOS-1900: optimization of point and click
    Used to be an html file. now is just a PDF. so templating not needed.

--- client/src/components/dialog/referenceAgreement/pointAndClickAgreement.ts
+++ client/src/components/dialog/referenceAgreement/pointAndClickAgreement.ts
@@ -1,18 +1,7 @@
-import type { ReferenceAgreement } from "demos-server";
-
-const POINT_AND_CLICK_AGREEMENT_TEMPLATE_ID =
-  "national-measure-stewards-terms-and-conditions";
-const POINT_AND_CLICK_AGREEMENT_NAME =
-  "National Measure Stewards Terms and Conditions";
-const POINT_AND_CLICK_AGREEMENT_FILE_NAME = "point-click-agreement.pdf";
-
 export const POINT_AND_CLICK_AGREEMENT = {
-  id: POINT_AND_CLICK_AGREEMENT_TEMPLATE_ID,
-  name: POINT_AND_CLICK_AGREEMENT_NAME,
+  id: "national-measure-stewards-terms-and-conditions",
+  name: "National Measure Stewards Terms and Conditions",
   createdAt: new Date("2026-08-17T00:00:00.000Z"),
-  fileName: POINT_AND_CLICK_AGREEMENT_FILE_NAME,
+  fileName: "point-click-agreement.pdf",
   url: "/reference-agreements/point-click-agreement.pdf",
-} satisfies Pick<ReferenceAgreement, "id" | "name" | "createdAt"> & {
-  fileName: string;
-  url: string;
-};
+} as const;
