ca51838b9ca4fe6975d15604a33bb1fc39bdbed2
Tom Rehman <tom.rehman@icf.com>
2026-08-26 13:39:33 -0400

    Update permissions.sql to reflect new read permissions for other schemas. File reorganized to make it a bit easier to read as well.

--- server/src/sql/permissions.sql
+++ server/src/sql/permissions.sql
@@ -27,39 +27,74 @@ BEGIN
 END;
 $$ LANGUAGE plpgsql;
 
--- Permission settings
--- Revoke all access to start
+-- Create the legacy_pmda schemas if not exists
+-- Helps to avoid permission issues later for lower environments where they don't exist
+CREATE SCHEMA IF NOT EXISTS legacy_pmda_raw;
+CREATE SCHEMA IF NOT EXISTS legacy_pmda_staged;
+
+-- Revoke all access by user / role
+-- PUBLIC revocations
 SELECT revoke_all_on_schema('public', 'PUBLIC');
 SELECT revoke_all_on_schema('demos_app', 'PUBLIC');
+SELECT revoke_all_on_schema('cron', 'PUBLIC');
+SELECT revoke_all_on_schema('legacy_pmda_raw', 'PUBLIC');
+SELECT revoke_all_on_schema('legacy_pmda_staged', 'PUBLIC');
+
+-- demos_read revocations
 SELECT revoke_all_on_schema('public', 'demos_read');
 SELECT revoke_all_on_schema('demos_app', 'demos_read');
+SELECT revoke_all_on_schema('cron', 'demos_read');
+SELECT revoke_all_on_schema('legacy_pmda_raw', 'demos_read');
+SELECT revoke_all_on_schema('legacy_pmda_staged', 'demos_read');
+
+-- demos_write revocations
 SELECT revoke_all_on_schema('public', 'demos_write');
 SELECT revoke_all_on_schema('demos_app', 'demos_write');
+SELECT revoke_all_on_schema('cron', 'demos_write');
+SELECT revoke_all_on_schema('legacy_pmda_raw', 'demos_write');
+SELECT revoke_all_on_schema('legacy_pmda_staged', 'demos_write');
+
+-- demos_delete revocations
 SELECT revoke_all_on_schema('public', 'demos_delete');
 SELECT revoke_all_on_schema('demos_app', 'demos_delete');
+SELECT revoke_all_on_schema('cron', 'demos_delete');
+SELECT revoke_all_on_schema('legacy_pmda_raw', 'demos_delete');
+SELECT revoke_all_on_schema('legacy_pmda_staged', 'demos_delete');
 
--- Grant usage on schemas
+-- Next, do grants per user / role
+-- Usage on schemas (and one on sequences), specific table access, change default privs
+-- PUBLIC grants
 GRANT USAGE ON SCHEMA public TO PUBLIC;
-GRANT USAGE ON SCHEMA demos_app TO demos_read;
-GRANT USAGE ON SCHEMA demos_app TO demos_write;
-GRANT USAGE ON SCHEMA demos_app TO demos_delete;
-
--- Give table access appropriately
 GRANT SELECT ON ALL TABLES IN SCHEMA public TO PUBLIC;
-GRANT SELECT ON ALL TABLES IN SCHEMA demos_app TO demos_read;
-GRANT INSERT, UPDATE ON ALL TABLES IN SCHEMA demos_app TO demos_write;
-GRANT DELETE ON ALL TABLES IN SCHEMA demos_app TO demos_delete;
-
--- Grant sequence usage for writing
-GRANT USAGE ON ALL SEQUENCES IN SCHEMA demos_app TO demos_write;
-
--- Update default privileges for future tables / drops and reloads
--- Eventually, all alter default privileges commands need to be executed by the table owners
 ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT SELECT ON TABLES TO PUBLIC;
+
+-- demos_read grants
+GRANT USAGE ON SCHEMA demos_app TO demos_read;
+GRANT USAGE ON SCHEMA cron TO demos_read;
+GRANT USAGE ON SCHEMA legacy_pmda_raw TO demos_read;
+GRANT USAGE ON SCHEMA legacy_pmda_staged TO demos_read;
+
+GRANT SELECT ON ALL TABLES IN SCHEMA demos_app TO demos_read;
+GRANT SELECT ON ALL TABLES IN SCHEMA cron TO demos_read;
+GRANT SELECT ON ALL TABLES IN SCHEMA legacy_pmda_raw TO demos_read;
+GRANT SELECT ON ALL TABLES IN SCHEMA legacy_pmda_staged TO demos_read;
+
 ALTER DEFAULT PRIVILEGES IN SCHEMA demos_app GRANT SELECT ON TABLES TO demos_read;
+ALTER DEFAULT PRIVILEGES IN SCHEMA cron GRANT SELECT ON TABLES TO demos_read;
+ALTER DEFAULT PRIVILEGES IN SCHEMA legacy_pmda_raw GRANT SELECT ON TABLES TO demos_read;
+ALTER DEFAULT PRIVILEGES IN SCHEMA legacy_pmda_staged GRANT SELECT ON TABLES TO demos_read;
+
+-- demos_write grants
+GRANT USAGE ON SCHEMA demos_app TO demos_write;
+GRANT USAGE ON ALL SEQUENCES IN SCHEMA demos_app TO demos_write;
+GRANT INSERT, UPDATE ON ALL TABLES IN SCHEMA demos_app TO demos_write;
 ALTER DEFAULT PRIVILEGES IN SCHEMA demos_app GRANT INSERT, UPDATE ON TABLES TO demos_write;
-ALTER DEFAULT PRIVILEGES IN SCHEMA demos_app GRANT DELETE ON TABLES TO demos_delete;
 ALTER DEFAULT PRIVILEGES IN SCHEMA demos_app GRANT USAGE ON SEQUENCES TO demos_write;
+
+-- demos_delete grants
+GRANT USAGE ON SCHEMA demos_app TO demos_delete;
+GRANT DELETE ON ALL TABLES IN SCHEMA demos_app TO demos_delete;
+ALTER DEFAULT PRIVILEGES IN SCHEMA demos_app GRANT DELETE ON TABLES TO demos_delete;
 
 -- Clean up utility function
 DROP FUNCTION revoke_all_on_schema;
