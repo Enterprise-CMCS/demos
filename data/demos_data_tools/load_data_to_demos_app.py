e16203dd3e36134265a16add9262e2655ff08bab
Tom Rehman <tom.rehman@icf.com>
2026-08-26 11:32:58 -0400

    DEMOS-2643-update-data-tools-for-future (#1748)
    * Removed the old PMDA exporter and began to update the DDB connection manager.
    
    * Updates to the migration schema manager to allow for using different DEMOS DB configs; need to do this for the data loader next.
    
    * WIP moving the loader to use runtime interpretation of strings
    
    * Further updates to allow configurable connection configs, a bit of type refactoring, tests need updated
    
    * Big rename and standardization pass over the Python code; basically all the tests are broken right now.
    
    * Added tests
    
    * Remove unused Dict import.

--- data/demos_data_tools/load_data_to_demos_app.py
+++ data/demos_data_tools/load_data_to_demos_app.py
@@ -1,19 +1,23 @@
 """Perform load actions based on a configuration from a data migration schema into demos_app."""
 
 import argparse
-import dataclasses
 import os
+from dataclasses import dataclass
 from logging import getLogger
-from typing import TYPE_CHECKING, Set, Tuple, assert_never
+from typing import Set, Tuple, assert_never, cast, get_args
 
 from dotenv import load_dotenv
 
-from duckdb_connection_manager import DEMOS_DDB_ATTACH_NAME, attach_demos_to_conn, create_duckdb_conn
-from load_data_to_demos_app_configs import (
-    AVAILABLE_DATA_LOAD_CONFIGURATIONS,
+from duckdb_connection_manager import (
+    DatabaseConfigurationName,
+    attach_db_to_duckdb_conn,
+    create_duckdb_conn,
+    get_attach_name_from_db_config_name,
 )
+from load_data_to_demos_app_configs import DataLoadConfigurationName, get_data_load_configuration
 from load_data_to_demos_app_types import (
     ArbitraryActionConfiguration,
+    ArbitrarySqlGenerationContext,
     DataLoadConfiguration,
     DataLoadSql,
     GeneratedArbitraryActionSql,
@@ -27,91 +31,110 @@ from load_data_to_demos_app_types import (
 )
 from logger_utils import config_logger
 
-if TYPE_CHECKING:
-    from argparse import Namespace
-
 logger = config_logger(getLogger(__name__))
 
 load_dotenv()
+DL_CONFIG_NAMES = get_args(DataLoadConfigurationName.__value__)
+DB_CONFIG_NAMES = get_args(DatabaseConfigurationName.__value__)
 
 
-def _parse_args() -> "Namespace":
+@dataclass(frozen=True)
+class CommandLineArguments:
+    """The command line arguments passed into the program."""
+
+    db_config_name: DatabaseConfigurationName
+    dl_config_name: DataLoadConfigurationName
+    dry_run: bool
+
+
+def _parse_args() -> CommandLineArguments:
     """Create argument parser and parse incoming arguments.
 
     Returns:
-        Namespace: The parsed argument namespace.
+        CommandLineArguments: The parsed argument namespace.
     """
-    available_configs = [field.name for field in dataclasses.fields(AVAILABLE_DATA_LOAD_CONFIGURATIONS)]
-    argparser = argparse.ArgumentParser()
-    argparser.add_argument(
-        "config_name",
-        choices=available_configs,
-        help="The name of a data load configuration specified in data_load_configurations.py",
+    parser = argparse.ArgumentParser(
+        description="Run data loads as part of the migration process",
+        formatter_class=lambda prog: argparse.HelpFormatter(prog, max_help_position=50),
     )
-    argparser.add_argument("--dry-run", "-d", action="store_true", help="Print generated SQL to console but do not run")
-    return argparser.parse_args()
+    parser.add_argument("db_config_name", choices=DB_CONFIG_NAMES, help="The name of the DB config to use")
+    parser.add_argument("dl_config_name", choices=DL_CONFIG_NAMES, help="The name of the data load config to use")
+    parser.add_argument("--dry-run", "-d", action="store_true", help="Print generated SQL to console but do not run")
+    parsed_args = parser.parse_args()
+    return CommandLineArguments(
+        db_config_name=cast(DatabaseConfigurationName, parsed_args.db_config_name),
+        dl_config_name=cast(DataLoadConfigurationName, parsed_args.dl_config_name),
+        dry_run=cast(bool, parsed_args.dry_run),
+    )
 
 
 def _generate_table_insert_sql(
-    source_schema: str, target_schema: str, insert_config: TableInsertActionConfiguration
+    source_schema: str,
+    target_schema: str,
+    db_config_name: DatabaseConfigurationName,
+    insert_config: TableInsertActionConfiguration,
 ) -> GeneratedInsertActionSql:
     """Generate an insert statement from a TableInsertActionConfiguration.
 
     Args:
         source_schema (str): The schema to load from.
         target_schema (str): The schema to load to.
+        db_config_name (DatabaseConfigurationName): The name of the DB config to use.
         insert_config (TableInsertActionConfiguration): The table configuration to be loaded.
 
     Returns:
         GeneratedInsertActionSql: The SQL query to be executed.
     """
-    logger.info(f"Generating insert statement for {insert_config.source_table} to {insert_config.destination_table}")
+    attach_name = get_attach_name_from_db_config_name(db_config_name)
+    logger.info(f"Generating insert statement for {insert_config.source_table} to {insert_config.target_table}")
     formatted_col_list = ", ".join(insert_config.column_list)
     query = f"""
         INSERT INTO
-            {DEMOS_DDB_ATTACH_NAME}.{target_schema}.{insert_config.destination_table}
+            {attach_name}.{target_schema}.{insert_config.target_table}
             ({formatted_col_list})
         SELECT
             {formatted_col_list}
         FROM
-            {DEMOS_DDB_ATTACH_NAME}.{source_schema}.{insert_config.source_table};
+            {attach_name}.{source_schema}.{insert_config.source_table};
     """
     return GeneratedInsertActionSql(insert_config, query)
 
 
-def _generate_trigger_action_sql(trigger_config: TriggerActionConfiguration) -> GeneratedTriggerActionSql:
+def _generate_trigger_action_sql(
+    db_config_name: DatabaseConfigurationName,
+    trigger_config: TriggerActionConfiguration,
+) -> GeneratedTriggerActionSql:
     """Generate an trigger action statement from a TriggerActionConfiguration.
 
-    Note: By design, this will always run against the APP_SCHEMA, as the other schemas do not have triggers.
-
     Args:
+        db_config_name (DatabaseConfigurationName): The name of the DB config to use.
         trigger_config (TriggerActionConfiguration): The trigger configuration to generate.
 
     Returns:
         GeneratedTriggerActionSql: The SQL query to be executed.
     """
-    app_schema = os.environ["APP_SCHEMA"]
+    attach_name = get_attach_name_from_db_config_name(db_config_name)
     logger.info(
         f"Generating control statement to {trigger_config.action_type} trigger "
-        f"{trigger_config.target_table}.{trigger_config.target_trigger_name}"
+        f"{trigger_config.trigger_table}.{trigger_config.trigger_name}"
     )
 
     if trigger_config.action_type == "disable":
         alter_statement = (
-            f"ALTER TABLE {app_schema}.{trigger_config.target_table} "
-            f"DISABLE TRIGGER {trigger_config.target_trigger_name};"
+            f"ALTER TABLE {trigger_config.trigger_schema}.{trigger_config.trigger_table} "
+            f"DISABLE TRIGGER {trigger_config.trigger_name};"
         )
     elif trigger_config.action_type == "enable":
         alter_statement = (
-            f"ALTER TABLE {app_schema}.{trigger_config.target_table} "
-            f"ENABLE TRIGGER {trigger_config.target_trigger_name};"
+            f"ALTER TABLE {trigger_config.trigger_schema}.{trigger_config.trigger_table} "
+            f"ENABLE TRIGGER {trigger_config.trigger_name};"
         )
     else:
         # This guards against the allowed values of a field expanding and causes it to be caught by type checking
         assert_never(trigger_config.action_type)
 
     # DuckDB has no concept of triggers, so we need to use the postgres_execute() function
-    query = f"CALL postgres_execute('{DEMOS_DDB_ATTACH_NAME}', '{alter_statement}')"
+    query = f"CALL postgres_execute('{attach_name}', '{alter_statement}')"
     return GeneratedTriggerActionSql(trigger_config, query)
 
 
@@ -137,53 +160,69 @@ def _generate_transaction_action_sql(transact_config: TransactionActionConfigura
 
 
 def _generate_arbitrary_action_sql(
-    arbitrary_action_config: ArbitraryActionConfiguration,
+    db_config_name: DatabaseConfigurationName, arbitrary_action_config: ArbitraryActionConfiguration
 ) -> GeneratedArbitraryActionSql:
     """Generate an arbitrary action statement from an ArbitraryActionConfiguration.
 
     Args:
+        db_config_name (DatabaseConfigurationName): The name of the DB config to use.
         arbitrary_action_config (ArbitraryActionConfiguration): The arbitrary action configuration to generate.
 
     Returns:
         GeneratedArbitraryActionSql: The SQL query to be executed.
     """
-    return GeneratedArbitraryActionSql(arbitrary_action_config, arbitrary_action_config.sql_query)
+    attach_name = get_attach_name_from_db_config_name(db_config_name)
+    app_schema = os.environ["APP_SCHEMA"]
+    sql_input = ArbitrarySqlGenerationContext(attach_name, app_schema)
+    sql_query = arbitrary_action_config.sql_generator(sql_input)
+    return GeneratedArbitraryActionSql(arbitrary_action_config, sql_query)
 
 
-def _generate_data_load_sql(data_load_config: DataLoadConfiguration) -> DataLoadSql:
+def _generate_data_load_sql(
+    db_config_name: DatabaseConfigurationName, data_load_config: DataLoadConfiguration
+) -> DataLoadSql:
     """Generate all the SQL for the data_load.
 
     Args:
-        data_load_config (DataLoadConfiguration): The full data load configuration.
+        db_config_name (DatabaseConfigurationName): The name of the DB config to use.
+        data_load_config (DataLoadConfiguration): The data load configuration to use.
 
     Returns:
         DataLoadSql: The SQL generated from the configuration.
     """
     generated_sql: DataLoadSql = []
     result: GeneratedSqlStatement
-    disabled_triggers: Set[Tuple[str, str]] = set()
-    for config in data_load_config.data_load_actions:
-        if isinstance(config, TableInsertActionConfiguration):
-            result = _generate_table_insert_sql(data_load_config.source_schema, data_load_config.target_schema, config)
-        elif isinstance(config, TriggerActionConfiguration):
-            if config.action_type == "disable":
-                disabled_triggers.add((config.target_table, config.target_trigger_name))
-            elif config.action_type == "enable":
-                disabled_triggers.remove((config.target_table, config.target_trigger_name))
+    disabled_triggers: Set[Tuple[str, str, str]] = set()
+    for action_config in data_load_config.data_load_actions:
+        if isinstance(action_config, TableInsertActionConfiguration):
+            result = _generate_table_insert_sql(
+                data_load_config.source_schema, data_load_config.target_schema, db_config_name, action_config
+            )
+        elif isinstance(action_config, TriggerActionConfiguration):
+            if action_config.action_type == "disable":
+                disabled_triggers.add(
+                    (action_config.trigger_schema, action_config.trigger_table, action_config.trigger_name)
+                )
+            elif action_config.action_type == "enable":
+                disabled_triggers.remove(
+                    (action_config.trigger_schema, action_config.trigger_table, action_config.trigger_name)
+                )
             else:
-                assert_never(config.action_type)
-            result = _generate_trigger_action_sql(config)
-        elif isinstance(config, TransactionActionConfiguration):
-            result = _generate_transaction_action_sql(config)
-        elif isinstance(config, ArbitraryActionConfiguration):
-            result = _generate_arbitrary_action_sql(config)
+                assert_never(action_config.action_type)
+            result = _generate_trigger_action_sql(db_config_name, action_config)
+        elif isinstance(action_config, TransactionActionConfiguration):
+            result = _generate_transaction_action_sql(action_config)
+        elif isinstance(action_config, ArbitraryActionConfiguration):
+            result = _generate_arbitrary_action_sql(db_config_name, action_config)
         else:
-            assert_never(config)
+            assert_never(action_config)
         generated_sql.append(result)
     if len(disabled_triggers) > 0:
         logger.warning("Note! Current configuration leaves some triggers disabled! Enabling them")
         for trigger in disabled_triggers:
-            result = _generate_trigger_action_sql(TriggerActionConfiguration("enable", trigger[0], trigger[1]))
+            result = _generate_trigger_action_sql(
+                db_config_name, TriggerActionConfiguration("enable", trigger[0], trigger[1], trigger[2])
+            )
             generated_sql.append(result)
     return generated_sql
 
@@ -200,12 +239,14 @@ def _create_log_execution_message_for_sql(sql_executed: GeneratedSqlStatement) -
     if isinstance(sql_executed, GeneratedInsertActionSql):
         return (
             f"Executing insert statement from {sql_executed.action_configuration.source_table} "
-            f"to {sql_executed.action_configuration.destination_table}"
+            f"to {sql_executed.action_configuration.target_table}"
         )
     elif isinstance(sql_executed, GeneratedTriggerActionSql):
         return (
             f"Executing SQL to {sql_executed.action_configuration.action_type} trigger "
-            f"{sql_executed.action_configuration.target_table}.{sql_executed.action_configuration.target_trigger_name}"
+            f"{sql_executed.action_configuration.trigger_schema}."
+            f"{sql_executed.action_configuration.trigger_table}."
+            f"{sql_executed.action_configuration.trigger_name}"
         )
     elif isinstance(sql_executed, GeneratedTransactionActionSql):
         return f"Executing {sql_executed.action_configuration.action_type} transaction statement"
@@ -215,15 +256,15 @@ def _create_log_execution_message_for_sql(sql_executed: GeneratedSqlStatement) -
         assert_never(sql_executed)
 
 
-def main(args: "Namespace") -> None:
+def main(args: CommandLineArguments) -> None:
     """Main program function."""
-    load_configuration = getattr(AVAILABLE_DATA_LOAD_CONFIGURATIONS, args.config_name)
-    generated_sql = _generate_data_load_sql(load_configuration)
+    data_load_config = get_data_load_configuration(args.dl_config_name)
+    generated_sql = _generate_data_load_sql(args.db_config_name, data_load_config)
     if args.dry_run:
         for query in generated_sql:
             logger.info(query.sql_query)
     else:
-        conn = attach_demos_to_conn(create_duckdb_conn())
+        conn = attach_db_to_duckdb_conn(create_duckdb_conn(), args.db_config_name)
         for query in generated_sql:
             logger.info(_create_log_execution_message_for_sql(query))
             conn.execute(query.sql_query)
--- data/demos_data_tools/manage_migration_schemas.py
+++ data/demos_data_tools/manage_migration_schemas.py
@@ -2,17 +2,21 @@
 
 import argparse
 import os
+from dataclasses import dataclass
 from logging import getLogger
-from typing import TYPE_CHECKING, Literal, get_args, assert_never
+from typing import TYPE_CHECKING, Literal, assert_never, cast, get_args
 
 from dotenv import load_dotenv
 
-from duckdb_connection_manager import DEMOS_DDB_ATTACH_NAME, create_duckdb_conn, attach_demos_to_conn
+from duckdb_connection_manager import (
+    DatabaseConfigurationName,
+    attach_db_to_duckdb_conn,
+    create_duckdb_conn,
+    get_attach_name_from_db_config_name,
+)
 from logger_utils import config_logger
 
 if TYPE_CHECKING:
-    from argparse import Namespace
-
     from duckdb import DuckDBPyConnection as DuckConn
 
 logger = config_logger(getLogger(__name__))
@@ -22,77 +26,107 @@ RAW_SCHEMA = os.environ["RAW_SCHEMA"]
 STAGING_SCHEMA = os.environ["STAGING_SCHEMA"]
 REV01_SCHEMA = os.environ["REV01_SCHEMA"]
 
-MigrationSchemaType = Literal["raw", "staging", "rev01"]
-MIGRATION_SCHEMAS = get_args(MigrationSchemaType)
+type MigrationSchemaName = Literal["raw", "staging", "rev01"]
+type MigrationSchemaAction = Literal["create", "drop"]
+MIGRATION_SCHEMA_NAMES = get_args(MigrationSchemaName.__value__)
+MIGRATION_SCHEMA_ACTIONS = get_args(MigrationSchemaAction.__value__)
+DB_CONFIG_NAMES = get_args(DatabaseConfigurationName.__value__)
 
 
-def _parse_args() -> "Namespace":
+@dataclass(frozen=True)
+class CommandLineArguments:
+    """The command line arguments passed into the program."""
+
+    db_config_name: DatabaseConfigurationName
+    schema_action: MigrationSchemaAction
+    schema_name: MigrationSchemaName
+
+
+def _parse_args() -> CommandLineArguments:
     """Create argument parser and parse incoming arguments.
 
     Returns:
-        Namespace: The parsed argument namespace.
+        CommandLineArguments: The parsed command line arguments.
     """
-    parser = argparse.ArgumentParser(description="Manage migration schemas for development use")
-    parser.add_argument("action", choices=["create", "drop"], help="Schema action to perform")
-    parser.add_argument("schema", choices=MIGRATION_SCHEMAS, help="Migration schema to manage")
-    return parser.parse_args()
+    parser = argparse.ArgumentParser(
+        description="Manage migration schemas for development use",
+        formatter_class=lambda prog: argparse.HelpFormatter(prog, max_help_position=50),
+    )
+    parser.add_argument("db_config_name", choices=DB_CONFIG_NAMES, help="The name of the DB config to use")
+    parser.add_argument("schema_action", choices=MIGRATION_SCHEMA_ACTIONS, help="The action to perform")
+    parser.add_argument("schema_name", choices=MIGRATION_SCHEMA_NAMES, help="The name of the schema to manage")
+    parsed_args = parser.parse_args()
+
+    return CommandLineArguments(
+        db_config_name=cast(DatabaseConfigurationName, parsed_args.db_config_name),
+        schema_action=cast(MigrationSchemaAction, parsed_args.schema_action),
+        schema_name=cast(MigrationSchemaName, parsed_args.schema_name),
+    )
 
 
-def _create_schema(conn: "DuckConn", which: MigrationSchemaType) -> None:
+def _create_schema(
+    conn: "DuckConn", db_config_name: DatabaseConfigurationName, schema_name: MigrationSchemaName
+) -> None:
     """Create one of the migration schemas.
 
     Args:
-        conn (DuckConn): A DuckDB connection with the DEMOS DB attached.
-        which (MigrationSchemaType): Which schema to create.
+        conn (DuckConn): A DuckDB connection with a DB attached.
+        db_config_name (DatabaseConfigurationName): The name of the DB config to use.
+        schema_name (MigrationSchemaName): The name of the schema to create.
     """
-    match which:
+    match schema_name:
         case "raw":
-            schema = RAW_SCHEMA
+            schema_to_create = RAW_SCHEMA
         case "staging":
-            schema = STAGING_SCHEMA
+            schema_to_create = STAGING_SCHEMA
         case "rev01":
-            schema = REV01_SCHEMA
+            schema_to_create = REV01_SCHEMA
         case _:
-            assert_never(which)
+            assert_never(schema_name)
+    demos_ddb_attach_name = get_attach_name_from_db_config_name(db_config_name)
 
-    logger.info(f"Attempting to create schema {schema}")
+    logger.info(f"Attempting to create schema {schema_to_create}")
     conn.execute(f"""
-        CREATE SCHEMA {DEMOS_DDB_ATTACH_NAME}.{schema};
+        CREATE SCHEMA {demos_ddb_attach_name}.{schema_to_create};
     """)
-    logger.info(f"Created schema {schema} successfully")
+    logger.info(f"Created schema {schema_to_create} successfully")
 
 
-def _drop_schema(conn: "DuckConn", which: MigrationSchemaType) -> None:
+def _drop_schema(conn: "DuckConn", db_config_name: DatabaseConfigurationName, schema_name: MigrationSchemaName) -> None:
     """Drop one of the migration schemas.
 
     Args:
-        conn (DuckConn): A DuckDB connection with the DEMOS DB attached.
-        which (MigrationSchemaType): Which schema to drop.
+        conn (DuckConn): A DuckDB connection with a DB attached.
+        db_config_name (DatabaseConfigurationName): The name of the DB config to use.
+        schema_name (MigrationSchemaName): The name of the schema to drop.
     """
-    match which:
+    match schema_name:
         case "raw":
-            schema = RAW_SCHEMA
+            schema_to_drop = RAW_SCHEMA
         case "staging":
-            schema = STAGING_SCHEMA
+            schema_to_drop = STAGING_SCHEMA
         case "rev01":
-            schema = REV01_SCHEMA
+            schema_to_drop = REV01_SCHEMA
         case _:
-            assert_never(which)
+            assert_never(schema_name)
+    demos_ddb_attach_name = get_attach_name_from_db_config_name(db_config_name)
 
-    logger.info(f"Attempting to drop schema {schema}")
+    logger.info(f"Attempting to drop schema {schema_to_drop}")
     conn.execute(f"""
-        DROP SCHEMA IF EXISTS {DEMOS_DDB_ATTACH_NAME}.{schema} CASCADE;
+        DROP SCHEMA IF EXISTS {demos_ddb_attach_name}.{schema_to_drop} CASCADE;
     """)
-    logger.info(f"Dropped schema {schema} successfully")
+    logger.info(f"Dropped schema {schema_to_drop} successfully")
 
 
-def main(args: "Namespace") -> None:
+def main(args: CommandLineArguments) -> None:
     """Main program function."""
-    conn = attach_demos_to_conn(create_duckdb_conn())
-    if args.action == "create":
-        _create_schema(conn, args.schema)
-    elif args.action == "drop":
-        _drop_schema(conn, args.schema)
+    conn = attach_db_to_duckdb_conn(create_duckdb_conn(), args.db_config_name)
+    if args.schema_action == "create":
+        _create_schema(conn, args.db_config_name, args.schema_name)
+    elif args.schema_action == "drop":
+        _drop_schema(conn, args.db_config_name, args.schema_name)
+    else:
+        assert_never(args.schema_action)
 
 
 if __name__ == "__main__":
