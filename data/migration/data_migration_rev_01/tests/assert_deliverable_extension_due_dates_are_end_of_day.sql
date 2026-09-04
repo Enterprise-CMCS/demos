94443ad574b9a45773eac14931a57812f7db7b5b
Connor J. Parke <connor.parke@icf.com>
2026-09-04 15:10:41 +0000

    DEMOS-2644: adjusted logic in error

--- data/migration/data_migration_rev_01/tests/assert_deliverable_extension_due_dates_are_end_of_day.sql
+++ data/migration/data_migration_rev_01/tests/assert_deliverable_extension_due_dates_are_end_of_day.sql
@@ -1,6 +1,10 @@
 SELECT *
 FROM {{ ref('final_demos_app_deliverable_extension') }}
 WHERE
-    original_date_requested IS NOT NULL
-    AND (original_date_requested AT TIME ZONE 'America/New_York')::TIME != TIME '23:59:59.999'
-    AND (final_date_granted AT TIME ZONE 'America/New_York')::TIME != TIME '23:59:59.999'
+    original_date_requested IS NULL
+    OR (original_date_requested AT TIME ZONE 'America/New_York')::TIME != TIME '23:59:59.999'
+    OR (
+        -- final_date_granted may be null (and, in this case, expected to be null), but if present it must be EOD
+        final_date_granted IS NOT NULL
+        AND (final_date_granted AT TIME ZONE 'America/New_York')::TIME != TIME '23:59:59.999'
+    )
