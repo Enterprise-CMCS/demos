f3b98deca0eb41a22dd92d1437a32b79b734d06e
Connor J. Parke <connor.parke@icf.com>
2026-09-10 21:18:44 +0000

    DEMOS-2644: cleaned up error

--- data/migration/data_migration_rev_01/models/errors/errors_delivs_crosswalked_with_no_final_demo.sql
+++ data/migration/data_migration_rev_01/models/errors/errors_delivs_crosswalked_with_no_final_demo.sql
@@ -10,4 +10,4 @@ SELECT *
 FROM
     {{ ref('deliverables_crosswalked_pmda_deliverables' ) }}
 WHERE
-    _legacy_mdcd_demo_id NOT IN (SELECT e1._legacy_mdcd_demo_id FROM finalized_demos AS e1)
+    _legacy_mdcd_demo_id NOT IN (SELECT finalized_demos._legacy_mdcd_demo_id FROM finalized_demos)
