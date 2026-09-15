b5f414de1d4945b03a3541ed78aa515ee25ae574
Tresdon Jones <tresdon.jones@icf.com>
2026-09-15 14:21:43 -0400

    update comment

--- server/src/flags.ts
+++ server/src/flags.ts
@@ -2,13 +2,13 @@ import semver from "semver";
 
 /**
  * Build-time feature flags and version constants.
- * Used in both local development (tsx) and production builds (esbuild).
  */
 
 export const __DEMOS_VERSION__ = "1.0.0";
 
 /**
  * Print version info on server startup.
- * Enabled when version > 0.0.1
+ * This is used as a sentinel to demonstrate how these flags should be used
+ * and should not be removed.
  */
 export const __FEATURE_PRINT_VERSION__ = semver.gt(__DEMOS_VERSION__, "0.0.1");
