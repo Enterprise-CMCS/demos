b80286a240368a9b7a70e4edf91fa74f9b492428
Lucas Nunes <nunesmlucasg@gmail.com>
2026-09-17 16:46:36 +0000

    DEMOS-2481: UI - View Associated Records for Type/Tag (#1833)
    * DEMOS-2481
    
    * updated close admin
    
    * feedback addressed
    
    ---------
    
    Co-authored-by: Lucas Nunes <lucas.nunes@icf.com>

---
+++ client/src/pages/admin/TypeTagAssociatedRecords.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { TertiaryButton } from "components/button";
+import { ChevronLeftIcon } from "components/icons";
+import { TabHeader } from "components/table/TabHeader";
+import { TypeTagAssociatedRecordsTable } from "components/table/tables/TypeTagAssociatedRecordsTable";
+import { useTypeTagSelection } from "./useTypeTagSelection";
+
+export const TYPE_TAG_ASSOCIATED_RECORDS_TEST_ID = "type-tag-associated-records";
+export const BACK_TO_TYPE_TAG_MANAGEMENT_BUTTON_NAME = "button-back-to-type-tag-management";
+
+export const TypeTagAssociatedRecords: React.FC = () => {
+  const { clearSelectedTypeTag } = useTypeTagSelection();
+
+  return (
+    <div className="flex flex-col gap-1" data-testid={TYPE_TAG_ASSOCIATED_RECORDS_TEST_ID}>
+      <div>
+        <TertiaryButton
+          name={BACK_TO_TYPE_TAG_MANAGEMENT_BUTTON_NAME}
+          aria-label="Back to Type/Tag Management"
+          size="small"
+          onClick={clearSelectedTypeTag}
+        >
+          <ChevronLeftIcon height="14" width="14" />
+          Type/Tag Management
+        </TertiaryButton>
+      </div>
+      <TabHeader title="Type/Tag File View" />
+      <TypeTagAssociatedRecordsTable />
+    </div>
+  );
+};
--- client/src/pages/admin/TypeTagManagement.tsx
+++ client/src/pages/admin/TypeTagManagement.tsx
@@ -1,5 +1,11 @@
 import React from "react";
+import { useSearchParams } from "react-router-dom";
+import { TypeTagTable } from "components/table/tables/TypeTagTable";
+import { TypeTagAssociatedRecords } from "./TypeTagAssociatedRecords";
+import { isTypeTagSelected } from "./useTypeTagSelection";
 
 export const TypeTagManagement: React.FC = () => {
-  return <div>Type/Tag Management</div>;
+  const [searchParams] = useSearchParams();
+
+  return isTypeTagSelected(searchParams) ? <TypeTagAssociatedRecords /> : <TypeTagTable />;
 };
