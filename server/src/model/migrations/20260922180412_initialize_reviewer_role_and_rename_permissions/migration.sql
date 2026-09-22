93338634cb2a7097a20c5c24b84dfcf4d8bc3f8e
Connor J. Parke <connor.parke@icf.com>
2026-09-22 19:00:31 +0000

    DEMOS-2841: added migration for role and permission

---
+++ server/src/model/migrations/20260922180412_initialize_reviewer_role_and_rename_permissions/migration.sql
@@ -0,0 +1,86 @@
+INSERT INTO demos_app.person_type (id) VALUES ('demos-cms-reviewer-user');
+
+INSERT INTO demos_app.user_person_type_limit (id) VALUES ('demos-cms-reviewer-user');
+
+
+
+INSERT INTO demos_app.role (id, grant_level_id) VALUES ('CMS Reviewer User', 'System');
+
+INSERT INTO demos_app.permission (id, grant_level_id) VALUES
+('Edit Documents on Assigned Deliverables', 'System'),
+('Delete Documents on Assigned Deliverables', 'System'),
+('Modify Applications', 'System'),
+('Modify Deliverables', 'System'),
+('Create Public Comment', 'System'),
+('Submit Deliverables', 'System'),
+('Request Deliverable Extensions', 'System'),
+('Modify Documents', 'System'),
+('Modify Deliverable CMS Documents', 'System'),
+('Modify Deliverable State Documents', 'System');
+
+INSERT INTO demos_app.role_permission (role_id, grant_level_id, permission_id) VALUES
+('Admin User', 'System', 'Edit Documents on Assigned Deliverables'),
+('Admin User', 'System', 'Delete Documents on Assigned Deliverables'),
+('Admin User', 'System', 'Modify Applications'),
+('Admin User', 'System', 'Modify Deliverables'),
+('Admin User', 'System', 'Create Public Comment'),
+('Admin User', 'System', 'Submit Deliverables'),
+('Admin User', 'System', 'Request Deliverable Extensions'),
+('Admin User', 'System', 'Modify Documents'),
+('Admin User', 'System', 'Modify Deliverable CMS Documents'),
+('Admin User', 'System', 'Modify Deliverable State Documents'),
+
+('CMS User', 'System', 'Edit Documents on Assigned Deliverables'),
+('CMS User', 'System', 'Delete Documents on Assigned Deliverables'),
+('CMS User', 'System', 'Modify Applications'),
+('CMS User', 'System', 'Modify Deliverables'),
+('CMS User', 'System', 'Create Public Comment'),
+('CMS User', 'System', 'Submit Deliverables'),
+('CMS User', 'System', 'Modify Documents'),
+('CMS User', 'System', 'Modify Deliverable CMS Documents'),
+('CMS User', 'System', 'Modify Deliverable State Documents'),
+
+('State User', 'System', 'Edit Documents on Assigned Deliverables'),
+('State User', 'System', 'Delete Documents on Assigned Deliverables'),
+('State User', 'System', 'Create Public Comment'),
+('State User', 'System', 'Submit Deliverables'),
+('State User', 'System', 'Request Deliverable Extensions'),
+('State User', 'System', 'Modify Deliverable State Documents'),
+
+('CMS Reviewer User', 'System', 'Edit All Documents'),
+('CMS Reviewer User', 'System', 'Delete All Documents'),
+('CMS Reviewer User', 'System', 'Modify Deliverables'),
+('CMS Reviewer User', 'System', 'Create Public Comment'),
+('CMS Reviewer User', 'System', 'Submit Deliverables'),
+('CMS Reviewer User', 'System', 'Modify Deliverable CMS Documents'),
+('CMS Reviewer User', 'System', 'Modify Deliverable State Documents'),
+('CMS Reviewer User', 'System', 'Access CMS Field'),
+('CMS Reviewer User', 'System', 'Access CMS Query'),
+('CMS Reviewer User', 'System', 'View All Demonstrations'),
+('CMS Reviewer User', 'System', 'View Assigned Demonstrations'),
+('CMS Reviewer User', 'System', 'View All Amendments'),
+('CMS Reviewer User', 'System', 'View Amendments on Assigned Demonstrations'),
+('CMS Reviewer User', 'System', 'View All Extensions'),
+('CMS Reviewer User', 'System', 'View Extensions on Assigned Demonstrations'),
+('CMS Reviewer User', 'System', 'View All Documents'),
+('CMS Reviewer User', 'System', 'View All Deliverables'),
+('CMS Reviewer User', 'System', 'View Deliverables on Assigned Demonstrations'),
+('CMS Reviewer User', 'System', 'View All DemonstrationRoleAssignments'),
+('CMS Reviewer User', 'System', 'View DemonstrationRoleAssignments on Assigned Demonstrations'),
+('CMS Reviewer User', 'System', 'View Documents on Assigned Deliverables'),
+('CMS Reviewer User', 'System', 'Generate On-Demand Report');
+
+DELETE FROM demos_app.role_permission
+WHERE permission_id IN (
+  'Edit State Documents on Assigned Deliverables',
+  'Delete State Documents on Assigned Deliverables',
+  'Perform CMS Action',
+  'Perform State Action'
+);
+
+DELETE FROM demos_app.permission WHERE id IN (
+  'Edit State Documents on Assigned Deliverables',
+  'Delete State Documents on Assigned Deliverables',
+  'Perform CMS Action',
+  'Perform State Action'
+);
