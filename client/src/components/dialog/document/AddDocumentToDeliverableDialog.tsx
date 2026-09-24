3760e4162de6bbd317e6345921a9f991be9cd517
dustbuster <dustin.h@globalalliantinc.com>
2026-09-24 20:17:23 +0000

    DEMOS-2787: Excluded State files from being BN Template
    There is no server constraints on this per the previous ticket and would be it's own effort

--- client/src/components/dialog/document/AddDocumentToDeliverableDialog.tsx
+++ client/src/components/dialog/document/AddDocumentToDeliverableDialog.tsx
@@ -54,6 +54,10 @@ const getCmsFileDocumentTypeSubset = (allowed: DocumentType[] = []): DocumentTyp
   return subset.length > 0 ? subset : [CMS_FILE_DEFAULT_DOCUMENT_TYPE];
 };
 
+const getStateFileDocumentTypeSubset = (
+  allowed: readonly DocumentType[] = DOCUMENT_TYPES
+): DocumentType[] => allowed.filter((type) => type !== "BN Template");
+
 interface AddDocumentToDeliverableDialogProps {
   onClose: () => void;
   deliverableId: string;
@@ -75,7 +79,7 @@ export const AddDocumentToDeliverableDialog: React.FC<AddDocumentToDeliverableDi
   const client = useApolloClient();
   const effectiveDocumentTypeSubset = isCmsFile
     ? getCmsFileDocumentTypeSubset(documentTypeSubset)
-    : (documentTypeSubset ?? DOCUMENT_TYPES).filter((type) => type !== "BN Template");
+    : getStateFileDocumentTypeSubset(documentTypeSubset);
   const { documentPassedVirusScan } = useDocumentPassedVirusScan();
   const { uploadDocument: uploadStateDocument } = useUploadDocument(
     UPLOAD_DOCUMENT_TO_DELIVERABLE_STATE_FILES_MUTATION
