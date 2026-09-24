eaca79bdc1682c234ef0927b503ba5fabc1d2d80
Tom Rehman <tom.rehman@icf.com>
2026-09-24 17:46:42 -0400

    typo

--- server/src/flags/getFeatureFlags.ts
+++ server/src/flags/getFeatureFlags.ts
@@ -5,7 +5,7 @@ export type DemosServerFeatureFlag = "printVersion" | "approveTagApi";
 export type DemosServerFeatureFlagSetting = Record<DemosServerFeatureFlag, boolean>;
 
 // This function exists to enable easier dependency injections
-//. Feature flags are added here
+// Feature flags are added here
 export function getFeatureFlags(currentVersion: DemosVersion): DemosServerFeatureFlagSetting {
   return {
     printVersion: semver.gte(currentVersion, "0.0.1"),
